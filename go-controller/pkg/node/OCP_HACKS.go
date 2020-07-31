5d78670f2f1d8385e5cd83cc29934c37597ba3db
Tim Rozet <trozet@redhat.com>
2020-07-30 20:31:01 -0400

    node: add iptables rules for certain cloud load balancers
    Certain CloudProviders (eg, Azure) assume that they can redirect
    loadbalancer traffic directly to a node with no rewriting, and the
    node will accept the packets. This works with kube-proxy because of a
    rule that was intended just as a shortcut to avoid sending local
    traffic to the load-balancer unnecessarily. Make it work with
    ovn-kubernetes too.
    
    Co-authored-by: Tim Rozet <trozet@redhat.com>
    Signed-off-by: Dan Winship <danw@redhat.com>
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- go-controller/pkg/node/OCP_HACKS.go
+++ go-controller/pkg/node/OCP_HACKS.go
@@ -2,14 +2,6 @@
 
 package node
 
-import (
-	"fmt"
-
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
-
-	kapi "k8s.io/api/core/v1"
-)
-
 // OCP HACK: Block MCS Access. https://github.com/openshift/ovn-kubernetes/pull/170
 func generateBlockMCSRules(rules *[]iptRule) {
 	*rules = append(*rules, iptRule{
@@ -36,34 +28,3 @@ func generateBlockMCSRules(rules *[]iptRule) {
 
 // END OCP HACK
 
-// OCP HACK: Fix Azure/GCP LoadBalancers. https://github.com/openshift/ovn-kubernetes/pull/112
-func getLoadBalancerIPTRules(svc *kapi.Service, svcPort kapi.ServicePort, gatewayIP string, targetPort int32) []iptRule {
-	var rules []iptRule
-	ingPort := fmt.Sprintf("%d", svcPort.Port)
-	for _, ing := range svc.Status.LoadBalancer.Ingress {
-		if ing.IP == "" {
-			continue
-		}
-		rules = append(rules, iptRule{
-			table: "nat",
-			chain: iptableNodePortChain,
-			args: []string{
-				"-d", ing.IP,
-				"-p", string(svcPort.Protocol), "--dport", ingPort,
-				"-j", "DNAT", "--to-destination", util.JoinHostPortInt32(gatewayIP, targetPort),
-			},
-		})
-		rules = append(rules, iptRule{
-			table: "filter",
-			chain: iptableNodePortChain,
-			args: []string{
-				"-d", ing.IP,
-				"-p", string(svcPort.Protocol), "--dport", ingPort,
-				"-j", "ACCEPT",
-			},
-		})
-	}
-	return rules
-}
-
-// END OCP HACK
