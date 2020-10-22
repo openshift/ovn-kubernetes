58cee847ac221be050d41ad2117117a5d8040c3b
Thomas F Herbert <therbert@redhat.com>
2020-10-22 17:47:10 -0400

    Modifications for klog v2.

--- go-controller/pkg/ovndbmanager/ovndbmanager.go
+++ go-controller/pkg/ovndbmanager/ovndbmanager.go
@@ -12,7 +12,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/kube"
--- go-controller/pkg/util/kube.go
+++ go-controller/pkg/util/kube.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 
 	"k8s.io/client-go/tools/record"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	kapi "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
