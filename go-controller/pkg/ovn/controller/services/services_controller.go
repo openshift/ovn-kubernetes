9afbdb042f74d067e41a452011112a5faafc738d
Antonio Ojea <aojea@redhat.com>
2020-11-24 12:28:59 +0100

    separate utils functions from the controller
    Signed-off-by: Antonio Ojea <aojea@redhat.com>

--- go-controller/pkg/ovn/controller/services/services_controller.go
+++ go-controller/pkg/ovn/controller/services/services_controller.go
@@ -13,7 +13,6 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 
 	coreinformers "k8s.io/client-go/informers/core/v1"
@@ -398,49 +397,4 @@ func serviceControllerKey(endpointSlice *discovery.EndpointSlice) (string, error
 		return "", fmt.Errorf("endpointSlice missing %s label", discovery.LabelServiceName)
 	}
 	return fmt.Sprintf("%s/%s", endpointSlice.Namespace, serviceName), nil
-}
-
-// return the endpoints that belong to the IPFamily as a slice of IP:Port
-func getLbEndpoints(slices []*discovery.EndpointSlice, protocol v1.Protocol, family v1.IPFamily) []string {
-	// return an empty object so the caller don't have to check for nil and can use it as an iterator
-	if len(slices) == 0 {
-		return []string{}
-	}
-	// Endpoint Slices are allowed to have duplicate endpoints
-	// we use a set to deduplicate endpoints
-	lbEndpoints := sets.NewString()
-	for _, slice := range slices {
-		klog.V(4).Infof("Getting endpoints for slice %s", slice.Name)
-		// Only return addresses that belong to the requested IP family
-		if slice.AddressType != discovery.AddressType(family) {
-			klog.V(4).Infof("Slice %s with different IP Family endpoints, requested: %s received: %s",
-				slice.Name, slice.AddressType, family)
-			continue
-		}
-
-		// build the list of endpoints in the slice
-		for _, port := range slice.Ports {
-			// Skip ports that doesn't match the protocol
-			if *port.Protocol != protocol {
-				klog.V(4).Infof("Slice %s with different Port protocol, requested: %s received: %s",
-					slice.Name, protocol, *port.Protocol)
-				continue
-			}
-
-			for _, endpoint := range slice.Endpoints {
-				// Skip endpoints that are not ready
-				if endpoint.Conditions.Ready != nil && !*endpoint.Conditions.Ready {
-					klog.V(4).Infof("Slice endpoints Not Ready")
-					continue
-				}
-				for _, ip := range endpoint.Addresses {
-					klog.V(4).Infof("Adding slice %s endpoints: %s %d", slice.Name, ip, *port.Port)
-					lbEndpoints.Insert(util.JoinHostPortInt32(ip, *port.Port))
-				}
-			}
-		}
-	}
-
-	klog.V(4).Infof("LB Endpoints for %s are: %v", slices[0].Labels[discovery.LabelServiceName], lbEndpoints.List())
-	return lbEndpoints.List()
 }
