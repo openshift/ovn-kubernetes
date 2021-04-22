8aee9e98512c91a3fdd0287a94a820d076823666
Tim Rozet <trozet@redhat.com>
2021-04-22 13:07:49 -0400

    Revert "removing the hybrid overlay externalGW code"
    This reverts commit b77e0e3027d905089daa50bcd2731e9b4498079e.
    
    (cherry picked from commit 2cd6c4ccd81a07955661fb43d4f54a8d2bf2ecda)

--- go-controller/pkg/ovn/ovn.go
+++ go-controller/pkg/ovn/ovn.go
@@ -87,6 +87,9 @@ type namespaceInfo struct {
 	// defines the namespaces egressFirewallPolicy
 	egressFirewallPolicy *egressFirewall
 
+	hybridOverlayExternalGW net.IP
+	hybridOverlayVTEP       net.IP
+
 	// routingExternalGWs is a slice of net.IP containing the values parsed from
 	// annotation k8s.ovn.org/routing-external-gws
 	routingExternalGWs gatewayInfo
