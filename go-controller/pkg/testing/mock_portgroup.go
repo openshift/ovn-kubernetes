45ca403748d4e7a1407264da6bc918cb0735724d
Casey Callendrello <cdc@redhat.com>
2021-06-07 16:05:14 +0200

    port groups: idempotency, bulk updates
    Updates mocks to reflect change in go-ovn, updates test cases.
    
    Update helper functions to allow for bulk operations.
    
    Signed-off-by: Casey Callendrello <cdc@redhat.com>

--- go-controller/pkg/testing/mock_portgroup.go
+++ go-controller/pkg/testing/mock_portgroup.go
@@ -2,6 +2,7 @@ package testing
 
 import (
 	"fmt"
+
 	goovn "github.com/ebay/go-ovn"
 	"k8s.io/klog/v2"
 )
@@ -41,7 +42,29 @@ func (mock *MockOVNClient) PortGroupAdd(group string, ports []string, external_i
 
 // Sets "ports" and/or "external_ids" on the port group named "group". It is an error if group does not exist.
 func (mock *MockOVNClient) PortGroupUpdate(group string, ports []string, external_ids map[string]string) (*goovn.OvnCommand, error) {
-	return nil, fmt.Errorf("method %s is not implemented yet", functionName())
+	var pg *goovn.PortGroup
+	if pg, _ = mock.PortGroupGet(group); pg == nil {
+		return nil, goovn.ErrorNotFound
+	}
+
+	// We don't yet support updating external-ids
+	if external_ids != nil {
+		return nil, fmt.Errorf("method %s is not implemented yet", functionName())
+	}
+
+	return &goovn.OvnCommand{
+		Exe: &MockExecution{
+			handler: mock,
+			op:      OpUpdate,
+			table:   PortGroupType,
+			objName: group,
+			objUpdate: UpdateCache{
+				FieldType:  PgLSPs,
+				FieldValue: ports,
+				UpdateOp:   OpUpdate,
+			},
+		},
+	}, nil
 }
 
 // Add port to port group.
@@ -180,6 +203,12 @@ func (mock *MockOVNClient) updatePortGroupCache(pgName string, update UpdateCach
 			pg.Ports = append(pg.Ports, fmt.Sprintf("%v", update.FieldValue))
 		case OpDelete:
 			pg.Ports = removeElement(pg.Ports, fmt.Sprintf("%v", update.FieldValue))
+		case OpUpdate:
+			ports, ok := update.FieldValue.([]string)
+			if !ok {
+				panic("invalid port group update...")
+			}
+			pg.Ports = ports
 		default:
 			return fmt.Errorf("unrecognized update op: %s", update.UpdateOp)
 		}
