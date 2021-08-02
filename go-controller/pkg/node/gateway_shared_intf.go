f8f777fb464ddafaa1250805f8a00c2cd23b932d
astoycos <astoycos@redhat.com>
2021-08-02 09:42:11 -0400

    Fix NodePort
    Fix a bug introduced by #2136 where the wrong
    iptables rules are programmed on the host for
    host -> svc -> regular pod Traffic flows
    
    Signed-off-by: astoycos <astoycos@redhat.com>

--- go-controller/pkg/node/gateway_shared_intf.go
+++ go-controller/pkg/node/gateway_shared_intf.go
@@ -385,7 +385,7 @@ func addServiceRules(service *kapi.Service, hasHostNet bool, npw *nodePortWatche
 			npw.updateServiceFlowCache(service, true, false)
 			npw.ofm.requestFlowSync()
 			if !npw.smartNICMode {
-				addSharedGatewayIptRules(service, true)
+				addSharedGatewayIptRules(service, false)
 			}
 			return
 		}
