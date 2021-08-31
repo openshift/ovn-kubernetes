fe08bf2b941afee78224333e91fd8db0c919e094
Dan Williams <dcbw@redhat.com>
2021-08-31 08:42:17 -0500

    go-ovn: make connect a struct method

--- go-controller/vendor/github.com/ebay/go-ovn/client.go
+++ go-controller/vendor/github.com/ebay/go-ovn/client.go
@@ -291,7 +291,7 @@ type ovndb struct {
 	reconn       bool
 }
 
-func connect(c *ovndb) (err error) {
+func (c *ovndb) connect() (err error) {
 	ovsdb, err := libovsdb.Connect(c.addr, c.tlsConfig)
 	if err != nil {
 		return err
@@ -349,7 +349,7 @@ func NewClient(cfg *Config) (Client, error) {
 		reconn:       cfg.Reconnect,
 	}
 
-	err := connect(ovndb)
+	err := ovndb.connect()
 	if err != nil {
 		return nil, err
 	}
@@ -362,7 +362,7 @@ func (c *ovndb) reconnect() {
 		klog.Infof("[%s] disconnected from %s; reconnecting ... ", c.db, c.addr)
 		retry := 0
 		for range ticker.C {
-			if err := connect(c); err != nil {
+			if err := c.connect(); err != nil {
 				if retry < 10 {
 					klog.Warningf("[%s] reconnect failed (%v); retry...", c.db, err)
 				} else if retry == 10 {
