trozet/ovn-kubernetes-1

Reduce nsInfo contention on external gateway ops
Previously nsInfo was holding not only a map of gateways per namespace,
but all of the routes per pod in an external gateway enabled namespace.
This means that during all external gateway route adds/deletes nsInfo
would need to be locked. This creates heavy contention in cluster
specifically using external gateway functionality.

This breaks out the pod routes portion into its own cache, which has
individual locks on a per pod basis. This allows exgw routes to be added
and removed without needing nsInfo lock. Additionally, since locks are
on a per pod basis, it provides less overall contention across the
cache.

Signed-off-by: Tim Rozet <trozet@redhat.com>

go-controller/pkg/ovn/namespace.go
@@ -313,7 +313,8 @@ func (oc *Controller) updateNamespace(old, newer *kapi.Namespace) {
 				}
 			}
 		} else {
-			oc.deleteGWRoutesForNamespace(nsInfo)
+			oc.deleteGWRoutesForNamespace(old.Namespace)
+			nsInfo.routingExternalGWs = gatewayInfo{}
 		}
 		exGateways, err := parseRoutingExternalGWAnnotation(gwAnnotation)
 		if err != nil {
@@ -461,7 +462,6 @@ func (oc *Controller) ensureNamespaceLocked(ns string, readOnly bool) (*namespac
 	if nsInfo == nil {
 		nsInfo = &namespaceInfo{
 			networkPolicies:       make(map[string]*networkPolicy),
-			podExternalRoutes:     make(map[string]map[string]string),
 			multicastEnabled:      false,
 			routingExternalPodGWs: make(map[string]gatewayInfo),
 		}

openshift/ovn-kubernetes

(cherry picked from commit c6db422df59d100ce5b9e7705a2989280d085cb9)

go-controller/pkg/ovn/namespace.go
@@ -287,7 +287,8 @@ func (oc *Controller) updateNamespace(old, newer *kapi.Namespace) {
 				}
 			}
 		} else {
-			oc.deleteGWRoutesForNamespace(nsInfo)
+			oc.deleteGWRoutesForNamespace(old.Name)
+			nsInfo.routingExternalGWs = gatewayInfo{}
 		}
 		exGateways, err := parseRoutingExternalGWAnnotation(gwAnnotation)
 		if err != nil {
@@ -371,7 +372,7 @@ func (oc *Controller) deleteNamespace(ns *kapi.Namespace) {
 		delete(nsInfo.networkPolicies, np.name)
 		oc.destroyNetworkPolicy(np, nsInfo)
 	}
-	oc.deleteGWRoutesForNamespace(nsInfo)
+	oc.deleteGWRoutesForNamespace(ns.Name)
 	oc.multicastDeleteNamespace(ns, nsInfo)
 }
 
@@ -436,7 +437,6 @@ func (oc *Controller) ensureNamespaceLocked(ns string, readOnly bool) (*namespac
 	if nsInfo == nil {
 		nsInfo = &namespaceInfo{
 			networkPolicies:       make(map[string]*networkPolicy),
-			podExternalRoutes:     make(map[string]map[string]string),
 			multicastEnabled:      false,
 			routingExternalPodGWs: make(map[string]gatewayInfo),
 		}