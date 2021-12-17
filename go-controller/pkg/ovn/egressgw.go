b2637658a9ee4231cd10e5b08eee58aae565fe73
Tim Rozet <trozet@redhat.com>
2021-12-17 15:08:17 -0500

    Multiple ExGW: fixes unique pod exgw cache key
    When pods are added to the cache as exgws for a namespace, only the
    pod's name is used as the key. This breaks a scenario where 2 pods with
    the same name are serving as exgws for the same namespace. Consider this
    example:
    
    1. app pod is created in ns foo
    2. exgwAPod is created in ns exgw1 (172.0.1.1), serving ns foo
    3. exgwAPod is created in ns exgw2 (172.0.1.2), serving ns foo
    
    In the above example, the app pod will only have one ECMP route for
    172.0.1.2, because the cache is keyed only on pod name.
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit a8017774a249e266ee1510ce072651597f663be0)
    (cherry picked from commit 2172c7424bbfdaa61f664d495149790f8c1c056e)
    (cherry picked from commit cf3adb220ef5eccd354bb53173ab49fd52f08ca2)
    (cherry picked from commit 18c8afe2d4cf6adfc6d7a3396b980d2050246a7b)

--- go-controller/pkg/ovn/egressgw.go
+++ go-controller/pkg/ovn/egressgw.go
@@ -176,7 +176,7 @@ func (oc *Controller) addPodExternalGWForNamespace(namespace string, pod *kapi.P
 	if err != nil {
 		return fmt.Errorf("failed to ensure namespace locked: %v", err)
 	}
-	nsInfo.routingExternalPodGWs[pod.Name] = egress
+	nsInfo.routingExternalPodGWs[makePodGWKey(pod)] = egress
 	existingGWs := sets.NewString()
 	for _, gwInfo := range nsInfo.routingExternalPodGWs {
 		existingGWs.Insert(gwInfo.gws.UnsortedList()...)
@@ -250,19 +250,20 @@ func (oc *Controller) deletePodExternalGW(pod *kapi.Pod) {
 	klog.Infof("Deleting routes for external gateway pod: %s, for namespace(s) %s", pod.Name,
 		podRoutingNamespaceAnno)
 	for _, namespace := range strings.Split(podRoutingNamespaceAnno, ",") {
-		oc.deletePodGWRoutesForNamespace(pod.Name, namespace)
+		oc.deletePodGWRoutesForNamespace(pod, namespace)
 	}
 }
 
 // deletePodGwRoutesForNamespace handles deleting all routes in a namespace for a specific pod GW
-func (oc *Controller) deletePodGWRoutesForNamespace(pod, namespace string) {
+func (oc *Controller) deletePodGWRoutesForNamespace(pod *kapi.Pod, namespace string) {
 	nsInfo, nsUnlock := oc.getNamespaceLocked(namespace, false)
 	if nsInfo == nil {
 		return
 	}
+	podGWKey := makePodGWKey(pod)
 	// check if any gateways were stored for this pod
-	foundGws, ok := nsInfo.routingExternalPodGWs[pod]
-	delete(nsInfo.routingExternalPodGWs, pod)
+	foundGws, ok := nsInfo.routingExternalPodGWs[podGWKey]
+	delete(nsInfo.routingExternalPodGWs, podGWKey)
 	existingGWs := sets.NewString()
 	for _, gwInfo := range nsInfo.routingExternalPodGWs {
 		existingGWs.Insert(gwInfo.gws.UnsortedList()...)
@@ -876,4 +877,8 @@ func buildOVNECMPCache() map[string][]*ovnRoute {
 		}
 	}
 	return ovnRouteCache
+}
+
+func makePodGWKey(pod *kapi.Pod) string {
+	return fmt.Sprintf("%s_%s", pod.Namespace, pod.Name)
 }
