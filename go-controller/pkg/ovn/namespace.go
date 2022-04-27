d55c5d09d5cc41eacc48cf9ef314713af570a675
Tim Rozet <trozet@redhat.com>
2022-04-27 17:22:37 -0400

    Fixes various issues with completed pods
    Changes-Include:
     - During pod sync we allocate IPs of all existing pods, but we should
       ignore completed pods
     - During namespace add we add all of the pod IPs to the ns address set,
       but we should ignore completed pods
     - During processing of delete event for a completed pod, we were trying
       to delete the pod again, which would try to free the IP that was
       previously released and could be in use by another pod. We should
       ignore delete events for completed resources as they would have been
       handled during update.
     - On node add, we add all existing pods on that node back to retry as
       an "add". We should skip completed pods here.
     - We now check during deletion of a completed pod (should happen on
       update only) to make sure no other running pods are using this IP as
       a failsafe to ensure we never release an IP in use by another pod or
       the related OVN config
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit 0182aa41338800a7708b747a3b5f795c2ec0dbea)
    (cherry picked from commit 98f70c150f475512fba8b55bcbd16b3bafc4a749)

--- go-controller/pkg/ovn/namespace.go
+++ go-controller/pkg/ovn/namespace.go
@@ -639,7 +639,7 @@ func (oc *Controller) createNamespaceAddrSetAllPods(ns string) (addressset.Addre
 	} else {
 		ips = make([]net.IP, 0, len(existingPods))
 		for _, pod := range existingPods {
-			if !pod.Spec.HostNetwork {
+			if !pod.Spec.HostNetwork && !util.PodCompleted(pod) {
 				podIPs, err := util.GetAllPodIPs(pod)
 				if err != nil {
 					klog.Warningf(err.Error())
