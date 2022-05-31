badec68efb1e2bc119b5cbc08a860dc904790cb9
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2022-05-31 20:42:51 +0200

    Fix egressIP object deletion if the node is deleted first
    There are a few instances in egressIP code where we shouldn't
    fail hard if objects are not found, example if a node gets
    deleted before egressIP watcher takes action. In this case,
    we can log a warning saying objects couldn't be deleted and
    move on since its technically not an error if the object was
    deleted by some other code path.
    
    In oder to ensure we don't leave behind stale reroute policy
    entries, we also add the logic of removing policies that have
    nexthop == joinIP of the node that's getting deleted into the
    gateway cleanup code.
    
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>
    (cherry picked from commit 95ab7596e6628aa035f078dcea9d878fd19eb607)
    (cherry picked from commit 95486d91cfe251fe10270a515f57d7a4b59234df)
    
     Conflicts:
    	go-controller/pkg/ovn/egressip.go
    	go-controller/pkg/util/net.go
    
    Conflicts because EIP retry code is absent in 4.10
    Note: Brought in the chnages done in retry code for
    getGatewayRouterJoinIP since they made more sense that way.

--- go-controller/pkg/util/net.go
+++ go-controller/pkg/util/net.go
@@ -5,11 +5,12 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"math/big"
 	"net"
 	"strconv"
 	"strings"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 
 	"github.com/ovn-org/libovsdb/client"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
@@ -86,7 +87,7 @@ func GetLRPAddrs(nbClient client.Client, portName string) ([]*net.IPNet, error) 
 	defer cancel()
 	err := nbClient.Get(ctx, &lrp)
 	if err != nil {
-		return nil, fmt.Errorf("unable to find router port: %s, err: %v", portName, err)
+		return nil, fmt.Errorf("unable to find router port %s: %w", portName, err)
 	}
 	gwLRPIPs := []*net.IPNet{}
 	for _, network := range lrp.Networks {
