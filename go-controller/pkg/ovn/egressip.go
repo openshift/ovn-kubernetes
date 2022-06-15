53330f9c2ee2911230f752289631d39708f24ae4
Flavio Fernandes <flaviof@redhat.com>
2022-06-15 01:54:23 +0000

    egressip: add metrics
    Add metrics to observe core events related to EgressIP:
    
    - name: egress_ips_assign_latency_seconds -- Histogram
      desc: The latency of egress IP assignment to ovn nb database
    
    - name: egress_ips_unassign_latency_seconds -- Histogram
      desc: The latency of egress IP unassignment from ovn nb database
    
    - name: egress_ips_node_unreachable_total -- Counter
      desc: The total number of times assigned egress IP(s) were unreachable
    
    - name: egress_ips_rebalance_total -- Counter
      desc: The total number of times assigned egress IP(s) needed to be moved to a different node
    
    Signed-off-by: Flavio Fernandes <flaviof@redhat.com>

--- go-controller/pkg/ovn/egressip.go
+++ go-controller/pkg/ovn/egressip.go
@@ -138,6 +138,11 @@ func (oc *Controller) reconcileEgressIP(old, new *egressipv1.EgressIP) (err erro
 		}
 	}
 
+	invalidStatusLen := len(invalidStatus)
+	if invalidStatusLen > 0 {
+		metrics.RecordEgressIPRebalance(invalidStatusLen)
+	}
+
 	// Add only the diff between what is requested and valid and that which
 	// isn't already assigned.
 	ipsToAssign := validSpecIPs
@@ -148,7 +153,7 @@ func (oc *Controller) reconcileEgressIP(old, new *egressipv1.EgressIP) (err erro
 		statusToKeep = append(statusToKeep, status)
 		ipsToAssign.Delete(status.EgressIP)
 	}
-	statusToRemove := make([]egressipv1.EgressIPStatusItem, 0, len(invalidStatus))
+	statusToRemove := make([]egressipv1.EgressIPStatusItem, 0, invalidStatusLen)
 	for status := range invalidStatus {
 		statusToRemove = append(statusToRemove, status)
 		ipsToRemove.Insert(status.EgressIP)
@@ -1159,6 +1164,7 @@ func (oc *Controller) syncEgressIPs(eIPs []interface{}) error {
 	// - Egress IPs which have been deleted while ovnkube-master was down
 	// - pods/namespaces which have stopped matching on egress IPs while
 	//   ovnkube-master was down
+
 	egressIPCache, err := oc.generateCacheForEgressIP(eIPs)
 	if err != nil {
 		return fmt.Errorf("syncEgressIPs unable to generate cache for egressip: %v", err)
@@ -1847,6 +1853,13 @@ type egressIPController struct {
 // (routing pod traffic to the egress node) and NAT objects on the egress node
 // (SNAT-ing to the egress IP).
 func (e *egressIPController) addPodEgressIPAssignment(egressIPName string, status egressipv1.EgressIPStatusItem, pod *kapi.Pod, podIPs []*net.IPNet) (err error) {
+	start := time.Now()
+	// FIXME(flaviof): add a flag to control when this metric is enabled
+	defer func() {
+		duration := time.Since(start)
+		metrics.RecordEgressIPAssign(duration)
+	}()
+
 	if err := e.deletePerPodGRSNAT(pod, podIPs, status); err != nil {
 		return err
 	}
@@ -1864,6 +1877,13 @@ func (e *egressIPController) addPodEgressIPAssignment(egressIPName string, statu
 // deletePodEgressIPAssignment deletes the OVN programmed egress IP
 // configuration mentioned for addPodEgressIPAssignment.
 func (e *egressIPController) deletePodEgressIPAssignment(egressIPName string, status egressipv1.EgressIPStatusItem, podIPs []*net.IPNet) error {
+	start := time.Now()
+	// FIXME(flaviof): add a flag to control when this metric is enabled
+	defer func() {
+		duration := time.Since(start)
+		metrics.RecordEgressIPUnassign(duration)
+	}()
+
 	if err := e.handleEgressReroutePolicy(podIPs, status, egressIPName, e.deleteEgressReroutePolicy); errors.Is(err, libovsdbclient.ErrNotFound) {
 		// if the gateway router join IP setup is already gone, then don't count it as error.
 		klog.Warningf("Unable to delete logical router policy, err: %v", err)
@@ -2094,6 +2114,7 @@ func (oc *Controller) checkEgressNodesReachability() {
 		oc.eIPC.allocator.Unlock()
 		for nodeName, shouldDelete := range reAddOrDelete {
 			if shouldDelete {
+				metrics.RecordEgressIPUnreachableNode()
 				klog.Warningf("Node: %s is detected as unreachable, deleting it from egress assignment", nodeName)
 				if err := oc.deleteEgressNode(nodeName); err != nil {
 					klog.Errorf("Node: %s is detected as unreachable, but could not re-assign egress IPs, err: %v", nodeName, err)
