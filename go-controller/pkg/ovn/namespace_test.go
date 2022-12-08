503a05ff31642856ac3ca43f60c6c5db351e7b86
Nadia Pinaeva <npinaeva@redhat.com>
2022-12-08 16:09:09 +0100

    Delete egress firewall acls before deleting referenced address sets to avoid ovn-controller syntax error.
    Signed-off-by: Nadia Pinaeva <npinaeva@redhat.com>
    (cherry picked from commit 66bb8fe1b3632f9d06b1afee6d3a0874d3bee1d0)

--- go-controller/pkg/ovn/namespace_test.go
+++ go-controller/pkg/ovn/namespace_test.go
@@ -2,7 +2,6 @@ package ovn
 
 import (
 	"context"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
 	"net"
 	"sync"
 
@@ -104,7 +103,7 @@ var _ = ginkgo.Describe("OVN Namespace Operations", func() {
 			// hybridNode-owned address set, should stay
 			fakeOvn.asf.NewAddressSet(ovntypes.HybridRoutePolicyPrefix+"node", []net.IP{net.ParseIP("1.1.1.5")})
 			// egress firewall-owned address set, should stay
-			// needs exiting ACL to distinguish from namespace-owned
+			// needs existing ACL to distinguish from namespace-owned
 			dnsAS, err := fakeOvn.asf.NewAddressSet("dnsname", []net.IP{net.ParseIP("1.1.1.6")})
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 			dnsHashName, _ := dnsAS.GetASHashNames()
