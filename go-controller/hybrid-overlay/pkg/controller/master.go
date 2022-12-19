openshift/ovn-kubernetes

commit 711237766b25e0728006eacb8dc1478b1ce359bb

ovn: track subnet allocations by owner and report conflicts as errors
Save an owner name for each allocation, and reject re-allocations/marks
that don't have the same owner.

Signed-off-by: Dan Williams <dcbw@redhat.com>
(cherry picked from commit f1917a08d94c34d0a3dc54a061d58c1cc407dffa)
(cherry picked from commit 2d7a0f7b524fed908917f1b705432fbee9eb29b7)

go-controller/hybrid-overlay/pkg/controller/master.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	libovsdbclient "github.com/ovn-org/libovsdb/client"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/types"
 	hotypes "github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/types"
 	houtil "github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/util"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
@@ -34,7 +33,7 @@ import (
 // MasterController is the master hybrid overlay controller
 type MasterController struct {
 	kube                  kube.Interface
-	allocator             subnetallocator.SubnetAllocator
+	allocator             *subnetallocator.HostSubnetAllocator
 	nodeEventHandler      informer.EventHandler
 	namespaceEventHandler informer.EventHandler
 	podEventHandler       informer.EventHandler
@@ -54,7 +53,7 @@ func NewMaster(kube kube.Interface,
 
 	m := &MasterController{
 		kube:      kube,
-		allocator: subnetallocator.NewSubnetAllocator(),
+		allocator: subnetallocator.NewHostSubnetAllocator(),
 		nbClient:  libovsdbNBClient,
 		sbClient:  libovsdbSBClient,
 	}
@@ -108,28 +107,10 @@ func NewMaster(kube kube.Interface,
 	)
 
 	// Add our hybrid overlay CIDRs to the subnetallocator
-	for _, clusterEntry := range config.HybridOverlay.ClusterSubnets {
-		err := m.allocator.AddNetworkRange(clusterEntry.CIDR, clusterEntry.HostSubnetLength)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	// Mark existing hostsubnets as already allocated
-	existingNodes, err := m.kube.GetNodes()
-	if err != nil {
-		return nil, fmt.Errorf("error in initializing/fetching subnets: %v", err)
-	}
-	for _, node := range existingNodes.Items {
-		hostsubnet, err := houtil.ParseHybridOverlayHostSubnet(&node)
-		if err != nil {
-			klog.Warningf(err.Error())
-		} else if hostsubnet != nil {
-			klog.V(5).Infof("Marking existing node %s hybrid overlay NodeSubnet %s as allocated", node.Name, hostsubnet)
-			if err := m.allocator.MarkAllocatedNetworks(hostsubnet); err != nil {
-				utilruntime.HandleError(err)
-			}
-		}
+	klog.Infof("Allocating subnets")
+	if err := m.allocator.InitRanges(config.Default.ClusterSubnets); err != nil {
+		klog.Errorf("Failed to initialize host subnet allocator ranges: %v", err)
+		return nil, err
 	}
 
 	return m, nil
@@ -175,31 +156,35 @@ func (m *MasterController) Run(stopCh <-chan struct{}) {
 // hybrid overlay subnet annotation. It returns any newly allocated subnet
 // or an error. If an error occurs, the newly allocated subnet will be released.
 func (m *MasterController) hybridOverlayNodeEnsureSubnet(node *kapi.Node, annotator kube.Annotator) (*net.IPNet, error) {
+	var existingSubnets []*net.IPNet
+
 	// Do not allocate a subnet if the node already has one
-	if subnet, _ := houtil.ParseHybridOverlayHostSubnet(node); subnet != nil {
-		return nil, nil
+	subnet, err := houtil.ParseHybridOverlayHostSubnet(node)
+	if err != nil {
+		// Log the error and try to allocate new subnets
+		klog.Infof("Failed to get node %s hybrid overlay subnet annotation: %v", node.Name, err)
+	} else if subnet != nil {
+		existingSubnets = []*net.IPNet{subnet}
 	}
 
 	// Allocate a new host subnet for this node
-	hostsubnets, err := m.allocator.AllocateNetworks()
+	// FIXME: hybrid overlay is only IPv4 for now due to limitations on the Windows side
+	hostSubnets, allocatedSubnets, err := m.allocator.AllocateNodeSubnets(node.Name, existingSubnets, true, false)
 	if err != nil {
 		return nil, fmt.Errorf("error allocating hybrid overlay HostSubnet for node %s: %v", node.Name, err)
 	}
 
-	if err := annotator.Set(types.HybridOverlayNodeSubnet, hostsubnets[0].String()); err != nil {
-		_ = m.allocator.ReleaseNetworks(hostsubnets[0])
+	if err := annotator.Set(hotypes.HybridOverlayNodeSubnet, hostSubnets[0].String()); err != nil {
+		_ = m.allocator.ReleaseNodeSubnets(node.Name, allocatedSubnets...)
 		return nil, err
 	}
 
-	klog.Infof("Allocated hybrid overlay HostSubnet %s for node %s", hostsubnets[0], node.Name)
-	return hostsubnets[0], nil
+	return hostSubnets[0], nil
 }
 
 func (m *MasterController) releaseNodeSubnet(nodeName string, subnet *net.IPNet) error {
-	if err := m.allocator.ReleaseNetworks(subnet); err != nil {
-		return fmt.Errorf("error deleting hybrid overlay HostSubnet %s for node %q: %s", subnet, nodeName, err)
-	}
-	klog.Infof("Deleted hybrid overlay HostSubnet %s for node %s", subnet, nodeName)
+	m.allocator.ReleaseAllNodeSubnets(nodeName)
+	klog.Infof("Deleted hybrid overlay HostSubnets for node %s", nodeName)
 	return nil
 }
 
@@ -216,7 +201,7 @@ func (m *MasterController) handleOverlayPort(node *kapi.Node, annotator kube.Ann
 	portName := util.GetHybridOverlayPortName(node.Name)
 
 	// retrieve mac annotation
-	am, annotationOK := node.Annotations[types.HybridOverlayDRMAC]
+	am, annotationOK := node.Annotations[hotypes.HybridOverlayDRMAC]
 	if annotationOK {
 		annotationMAC, err = net.ParseMAC(am)
 		if err != nil {
@@ -232,7 +217,7 @@ func (m *MasterController) handleOverlayPort(node *kapi.Node, annotator kube.Ann
 		klog.V(5).Infof("No subnet allocation yet for %s", node.Name)
 		if annotationOK {
 			m.deleteOverlayPort(node)
-			annotator.Delete(types.HybridOverlayDRMAC)
+			annotator.Delete(hotypes.HybridOverlayDRMAC)
 		}
 		return nil
 	}
@@ -304,7 +289,7 @@ func (m *MasterController) handleOverlayPort(node *kapi.Node, annotator kube.Ann
 
 	if !annotationOK {
 		klog.Infof("Setting node %s hybrid overlay mac annotation to %s", node.Name, annotationMAC.String())
-		if err := annotator.Set(types.HybridOverlayDRMAC, portMAC.String()); err != nil {
+		if err := annotator.Set(hotypes.HybridOverlayDRMAC, portMAC.String()); err != nil {
 			return fmt.Errorf("failed to set node %s hybrid overlay DRMAC annotation: %v", node.Name, err)
 		}
 	}
@@ -359,7 +344,7 @@ func (m *MasterController) DeleteNode(node *kapi.Node) error {
 		}
 	}
 
-	if _, ok := node.Annotations[types.HybridOverlayDRMAC]; ok && !houtil.IsHybridOverlayNode(node) {
+	if _, ok := node.Annotations[hotypes.HybridOverlayDRMAC]; ok && !houtil.IsHybridOverlayNode(node) {
 		m.deleteOverlayPort(node)
 	}
 

go-controller/pkg/ovn/master.go
@@ -821,7 +821,13 @@ func (oc *Controller) addNodeAnnotations(node *kapi.Node, hostSubnets []*net.IPN
 }
 
 func (oc *Controller) addNode(node *kapi.Node) ([]*net.IPNet, error) {
-	hostSubnets, allocatedSubnets, err := oc.masterSubnetAllocator.AllocateNodeSubnets(node, config.IPv4Mode, config.IPv6Mode)
+	existingSubnets, err := util.ParseNodeHostSubnetAnnotation(node)
+	if err != nil && !util.IsAnnotationNotSetError(err) {
+		// Log the error and try to allocate new subnets
+		klog.Infof("Failed to get node %s host subnets annotations: %v", node.Name, err)
+	}
+
+	hostSubnets, allocatedSubnets, err := oc.masterSubnetAllocator.AllocateNodeSubnets(node.Name, existingSubnets, config.IPv4Mode, config.IPv6Mode)
 	if err != nil {
 		return nil, err
 	}
@@ -927,10 +933,8 @@ func (oc *Controller) deleteNodeLogicalNetwork(nodeName string) error {
 	return nil
 }
 
-func (oc *Controller) deleteNode(nodeName string, hostSubnets []*net.IPNet) error {
-	if err := oc.masterSubnetAllocator.ReleaseNodeSubnets(nodeName, hostSubnets...); err != nil {
-		return fmt.Errorf("error releasing node %s subnets: %v", nodeName, err)
-	}
+func (oc *Controller) deleteNode(nodeName string) error {
+	oc.masterSubnetAllocator.ReleaseAllNodeSubnets(nodeName)
 
 	if err := oc.deleteNodeLogicalNetwork(nodeName); err != nil {
 		return fmt.Errorf("error deleting node %s logical network: %v", nodeName, err)
@@ -1077,42 +1081,11 @@ func (oc *Controller) syncNodes(nodes []interface{}) error {
 	if err != nil {
 		return fmt.Errorf("failed to get node logical switches which have other-config set: %v", err)
 	}
-	if len(nodeSwitches) == 0 {
-		klog.Warning("Did not find any logical switches with other-config")
-	}
-
 	for _, nodeSwitch := range nodeSwitches {
-		if foundNodes.Has(nodeSwitch.Name) {
-			// node still exists, no cleanup to do
-			continue
-		}
-
-		var subnets []*net.IPNet
-		for key, value := range nodeSwitch.OtherConfig {
-			var subnet *net.IPNet
-			if key == "subnet" {
-				_, subnet, err = net.ParseCIDR(value)
-				if err != nil {
-					klog.Warningf("Unable to parse subnet CIDR %v", value)
-					continue
-				}
-			} else if key == "ipv6_prefix" {
-				_, subnet, err = net.ParseCIDR(value + "/64")
-				if err != nil {
-					klog.Warningf("Unable to parse ipv6_prefix CIDR %v/64", value)
-					continue
-				}
+		if !foundNodes.Has(nodeSwitch.Name) {
+			if err := oc.deleteNode(nodeSwitch.Name); err != nil {
+				return fmt.Errorf("failed to delete node:%s, err:%v", nodeSwitch.Name, err)
 			}
-			if subnet != nil {
-				subnets = append(subnets, subnet)
-			}
-		}
-		if len(subnets) == 0 {
-			continue
-		}
-
-		if err := oc.deleteNode(nodeSwitch.Name, subnets); err != nil {
-			return fmt.Errorf("failed to delete node:%s, err:%v", nodeSwitch.Name, err)
 		}
 	}
 
@@ -1259,8 +1232,7 @@ func (oc *Controller) deleteNodeEvent(node *kapi.Node) error {
 	klog.V(5).Infof("Deleting Node %q. Removing the node from "+
 		"various caches", node.Name)
 
-	nodeSubnets, _ := util.ParseNodeHostSubnetAnnotation(node)
-	if err := oc.deleteNode(node.Name, nodeSubnets); err != nil {
+	if err := oc.deleteNode(node.Name); err != nil {
 		return err
 	}
 	oc.lsManager.DeleteNode(node.Name)

go-controller/pkg/ovn/master_test.go
@@ -854,20 +854,22 @@ subnet=%s
 
 var _ = ginkgo.Describe("Gateway Init Operations", func() {
 	var (
-		app             *cli.App
-		f               *factory.WatchFactory
-		stopChan        chan struct{}
-		wg              *sync.WaitGroup
-		libovsdbCleanup *libovsdbtest.Cleanup
+		app      *cli.App
+		f        *factory.WatchFactory
+		stopChan chan struct{}
+		wg       *sync.WaitGroup
 
-		dbSetup                                  libovsdbtest.TestSetup
-		node1                                    tNode
-		testNode                                 v1.Node
-		fakeClient                               *util.OVNClientset
-		kubeFakeClient                           *fake.Clientset
-		clusterController                        *Controller
-		nodeAnnotator                            kube.Annotator
-		libovsdbOvnNBClient, libovsdbOvnSBClient libovsdbclient.Client
+		libovsdbOvnNBClient libovsdbclient.Client
+		libovsdbOvnSBClient libovsdbclient.Client
+		libovsdbCleanup     *libovsdbtest.Cleanup
+
+		dbSetup           libovsdbtest.TestSetup
+		node1             tNode
+		testNode          v1.Node
+		fakeClient        *util.OVNClientset
+		kubeFakeClient    *fake.Clientset
+		clusterController *Controller
+		nodeAnnotator     kube.Annotator
 	)
 
 	const (

go-controller/pkg/ovn/subnetallocator/allocator.go
@@ -13,14 +13,18 @@ var ErrSubnetAllocatorFull = fmt.Errorf("no subnets available.")
 
 type SubnetAllocator interface {
 	AddNetworkRange(network *net.IPNet, hostSubnetLen int) error
-	MarkAllocatedNetworks(...*net.IPNet) error
+	MarkAllocatedNetworks(string, ...*net.IPNet) error
 	// Usage returns the number of available and used v4 subnets, and
 	// the number of available and used v6 subnets
 	Usage() (uint64, uint64, uint64, uint64)
-	AllocateNetworks() ([]*net.IPNet, error)
-	AllocateIPv4Network() (*net.IPNet, error)
-	AllocateIPv6Network() (*net.IPNet, error)
-	ReleaseNetworks(...*net.IPNet) error
+	AllocateNetworks(string) ([]*net.IPNet, error)
+	AllocateIPv4Network(string) (*net.IPNet, error)
+	AllocateIPv6Network(string) (*net.IPNet, error)
+	// ReleaseNetworks releases the given networks if they are owned by the
+	// given owner
+	ReleaseNetworks(string, ...*net.IPNet) error
+	// ReleaseAllNetworks releases all networks owned by the given owner
+	ReleaseAllNetworks(string)
 }
 
 type BaseSubnetAllocator struct {
@@ -54,7 +58,7 @@ func (sna *BaseSubnetAllocator) Usage() (uint64, uint64, uint64, uint64) {
 }
 
 // AddNetworkRange makes the given range available for allocation and returns
-// the number of IPv4 or IPv6 subnets in the range.
+// nil, or an error on failure.
 func (sna *BaseSubnetAllocator) AddNetworkRange(network *net.IPNet, hostSubnetLen int) error {
 	sna.Lock()
 	defer sna.Unlock()
@@ -72,31 +76,43 @@ func (sna *BaseSubnetAllocator) AddNetworkRange(network *net.IPNet, hostSubnetLe
 	return nil
 }
 
-func (sna *BaseSubnetAllocator) MarkAllocatedNetworks(subnets ...*net.IPNet) error {
+// MarkAllocatedNetworks will mark the given subnets as already allocated by
+// the given owner. Marking is all-or-nothing; if marking one of the subnets
+// fails then none of them are marked as allocated.
+func (sna *BaseSubnetAllocator) MarkAllocatedNetworks(owner string, subnets ...*net.IPNet) error {
 	sna.Lock()
 	defer sna.Unlock()
 
+	// Track marked subnets so that we can release any already-marked
+	// subnets if a later one fails.
 	releaseOnError := make([]*net.IPNet, 0, len(subnets))
+
 	for i := range subnets {
 		marked := false
 		for _, snr := range sna.v4ranges {
-			if snr.markAllocatedNetwork(subnets[i]) {
+			if ok, err := snr.markAllocatedNetwork(owner, subnets[i]); ok {
 				releaseOnError = append(releaseOnError, subnets[i])
 				marked = true
 				break
+			} else if err != nil {
+				_ = sna.releaseNetworks(owner, releaseOnError...)
+				return err
 			}
 		}
 		if !marked {
 			for _, snr := range sna.v6ranges {
-				if snr.markAllocatedNetwork(subnets[i]) {
+				if ok, err := snr.markAllocatedNetwork(owner, subnets[i]); ok {
 					releaseOnError = append(releaseOnError, subnets[i])
 					marked = true
 					break
+				} else if err != nil {
+					_ = sna.releaseNetworks(owner, releaseOnError...)
+					return err
 				}
 			}
 		}
 		if !marked {
-			_ = sna.releaseNetworks(releaseOnError...)
+			_ = sna.releaseNetworks(owner, releaseOnError...)
 			return fmt.Errorf("network %s does not belong to any known range", subnets[i].String())
 		}
 	}
@@ -104,21 +120,21 @@ func (sna *BaseSubnetAllocator) MarkAllocatedNetworks(subnets ...*net.IPNet) err
 }
 
 // AllocateNetworks tries to allocate networks in all the ranges available
-func (sna *BaseSubnetAllocator) AllocateNetworks() ([]*net.IPNet, error) {
+func (sna *BaseSubnetAllocator) AllocateNetworks(owner string) ([]*net.IPNet, error) {
 	var networks []*net.IPNet
 	var err error
-	ipv4network, err := sna.AllocateIPv4Network()
+	ipv4network, err := sna.AllocateIPv4Network(owner)
 	if err != nil {
 		return nil, err
 	}
 	if ipv4network != nil {
 		networks = append(networks, ipv4network)
 	}
-	ipv6network, err := sna.AllocateIPv6Network()
+	ipv6network, err := sna.AllocateIPv6Network(owner)
 	if err != nil {
 		// Release already allocated networks on error
 		if len(networks) > 0 {
-			_ = sna.ReleaseNetworks(networks...)
+			_ = sna.ReleaseNetworks(owner, networks...)
 		}
 		return nil, err
 	}
@@ -129,14 +145,14 @@ func (sna *BaseSubnetAllocator) AllocateNetworks() ([]*net.IPNet, error) {
 }
 
 // AllocateIPv4Network tries to allocate an IPv4 network if there are ranges available
-func (sna *BaseSubnetAllocator) AllocateIPv4Network() (*net.IPNet, error) {
+func (sna *BaseSubnetAllocator) AllocateIPv4Network(owner string) (*net.IPNet, error) {
 	sna.Lock()
 	defer sna.Unlock()
 	if len(sna.v4ranges) == 0 {
 		return nil, nil
 	}
 	for _, snr := range sna.v4ranges {
-		sn := snr.allocateNetwork()
+		sn := snr.allocateNetwork(owner)
 		if sn != nil {
 			return sn, nil
 		}
@@ -145,14 +161,14 @@ func (sna *BaseSubnetAllocator) AllocateIPv4Network() (*net.IPNet, error) {
 }
 
 // AllocateIPv6Network tries to allocate an IPv6 network if there are ranges available
-func (sna *BaseSubnetAllocator) AllocateIPv6Network() (*net.IPNet, error) {
+func (sna *BaseSubnetAllocator) AllocateIPv6Network(owner string) (*net.IPNet, error) {
 	sna.Lock()
 	defer sna.Unlock()
 	if len(sna.v6ranges) == 0 {
 		return nil, nil
 	}
 	for _, snr := range sna.v6ranges {
-		sn := snr.allocateNetwork()
+		sn := snr.allocateNetwork(owner)
 		if sn != nil {
 			return sn, nil
 		}
@@ -160,36 +176,65 @@ func (sna *BaseSubnetAllocator) AllocateIPv6Network() (*net.IPNet, error) {
 	return nil, ErrSubnetAllocatorFull
 }
 
-func (sna *BaseSubnetAllocator) ReleaseNetworks(subnets ...*net.IPNet) error {
+func (sna *BaseSubnetAllocator) ReleaseNetworks(owner string, subnets ...*net.IPNet) error {
 	sna.Lock()
 	defer sna.Unlock()
-	return sna.releaseNetworks(subnets...)
+	return sna.releaseNetworks(owner, subnets...)
 }
 
-func (sna *BaseSubnetAllocator) releaseNetworks(subnets ...*net.IPNet) error {
+func (sna *BaseSubnetAllocator) ReleaseAllNetworks(owner string) {
+	sna.Lock()
+	defer sna.Unlock()
+	sna.releaseAllNetworks(owner)
+}
+
+// releaseNetworks attempts to release all given subnets, even if a failure
+// occurs during release. It returns nil, or an aggregate error for any
+// failures that occurred.
+func (sna *BaseSubnetAllocator) releaseNetworks(owner string, subnets ...*net.IPNet) error {
 	var errorList []error
 	for _, subnet := range subnets {
-		released := false
+		var released, errHandled, ok bool
+		var err error
 		for _, snr := range sna.v4ranges {
-			if snr.releaseNetwork(subnet) {
+			ok, err = snr.releaseNetwork(owner, subnet)
+			if ok {
 				released = true
 				break
+			} else if err != nil {
+				errHandled = true
+				errorList = append(errorList, err)
 			}
 		}
-		if !released {
+		if !released && !errHandled {
 			for _, snr := range sna.v6ranges {
-				if snr.releaseNetwork(subnet) {
+				ok, err = snr.releaseNetwork(owner, subnet)
+				if ok {
 					released = true
 					break
+				} else if err != nil {
+					errorList = append(errorList, err)
 				}
 			}
 		}
-		if !released {
+		if !released && !errHandled {
 			errorList = append(errorList, fmt.Errorf("network %s does not belong to any known range", subnet.String()))
 		}
 	}
 
 	return utilerrors.NewAggregate(errorList)
+}
+
+// releaseAllNetworks attempts to release all subnets of a given owner, even
+// if a failure occurs during release. It returns nil, or an aggregate error
+// for any failures that occurred.
+func (sna *BaseSubnetAllocator) releaseAllNetworks(owner string) {
+	for _, snr := range sna.v4ranges {
+		snr.releaseAllNetworks(owner)
+	}
+	for _, snr := range sna.v6ranges {
+		snr.releaseAllNetworks(owner)
+	}
 }
 
 // subnetAllocatorRange handles allocating subnets out of a single CIDR
@@ -198,7 +243,7 @@ type subnetAllocatorRange struct {
 	hostBits   uint32
 	subnetBits uint32
 	next       uint32
-	allocMap   map[string]bool
+	allocMap   map[string]string
 	used       uint32
 
 	// IPv4-only address-alignment hackery; see below
@@ -223,7 +268,7 @@ func newSubnetAllocatorRange(network *net.IPNet, hostSubnetLen int) (*subnetAllo
 		hostBits:   hostBits,
 		subnetBits: subnetBits,
 		next:       0,
-		allocMap:   make(map[string]bool),
+		allocMap:   make(map[string]string),
 	}
 
 	// In the simple case, the subnet part of the 32-bit IP address is just the subnet
@@ -263,19 +308,44 @@ func (snr *subnetAllocatorRange) usage() (uint64, uint64) {
 	return one << snr.subnetBits, uint64(snr.used)
 }
 
+type alreadyOwnedError struct {
+	network       string
+	existingOwner string
+}
+
+func (e alreadyOwnedError) Error() string {
+	return fmt.Sprintf("network %s already owned by %s", e.network, e.existingOwner)
+}
+
+// IsAlreadyOwnedError returns true if the error indicates the network was already owned
+func IsAlreadyOwnedError(err error) bool {
+	_, ok := err.(alreadyOwnedError)
+	return ok
+}
+
 // markAllocatedNetwork marks network as being in use, if it is part of snr's range.
-// It returns whether the network was in snr's range.
-func (snr *subnetAllocatorRange) markAllocatedNetwork(network *net.IPNet) bool {
+// It returns whether the network was in snr's range, and returns an error if
+// network was already allocated to a different owner.
+func (snr *subnetAllocatorRange) markAllocatedNetwork(owner string, network *net.IPNet) (bool, error) {
 	str := network.String()
-	if snr.network.Contains(network.IP) {
-		snr.allocMap[str] = true
-		snr.used++
+	if !snr.network.Contains(network.IP) {
+		return false, nil
 	}
-	return snr.allocMap[str]
+
+	existingOwner, ok := snr.allocMap[str]
+	if !ok {
+		snr.allocMap[str] = owner
+		snr.used++
+		return true, nil
+	} else if existingOwner == owner {
+		return true, nil
+	}
+
+	return false, alreadyOwnedError{str, existingOwner}
 }
 
 // allocateNetwork returns a new subnet, or nil if the range is full
-func (snr *subnetAllocatorRange) allocateNetwork() *net.IPNet {
+func (snr *subnetAllocatorRange) allocateNetwork(owner string) *net.IPNet {
 	netMaskSize, addrLen := snr.network.Mask.Size()
 	numSubnets := uint32(1) << snr.subnetBits
 	if snr.subnetBits > 24 {
@@ -312,8 +382,8 @@ func (snr *subnetAllocatorRange) allocateNetwork() *net.IPNet {
 		}
 
 		genSubnet := &net.IPNet{IP: genIP, Mask: net.CIDRMask(int(snr.subnetBits)+netMaskSize, addrLen)}
-		if !snr.allocMap[genSubnet.String()] {
-			snr.allocMap[genSubnet.String()] = true
+		if _, ok := snr.allocMap[genSubnet.String()]; !ok {
+			snr.allocMap[genSubnet.String()] = owner
 			snr.next = n + 1
 			snr.used++
 			return genSubnet
@@ -326,12 +396,30 @@ func (snr *subnetAllocatorRange) allocateNetwork() *net.IPNet {
 
 // releaseNetwork marks network as being not in use, if it is part of snr's range.
 // It returns whether the network was in snr's range.
-func (snr *subnetAllocatorRange) releaseNetwork(network *net.IPNet) bool {
+func (snr *subnetAllocatorRange) releaseNetwork(owner string, network *net.IPNet) (bool, error) {
 	if !snr.network.Contains(network.IP) {
-		return false
+		return false, nil
 	}
 
-	snr.allocMap[network.String()] = false
-	snr.used--
-	return true
+	str := network.String()
+	existingOwner, ok := snr.allocMap[str]
+	if !ok {
+		return false, nil
+	} else if existingOwner == owner {
+		delete(snr.allocMap, str)
+		snr.used--
+		return true, nil
+	}
+
+	return false, alreadyOwnedError{str, existingOwner}
+}
+
+// releaseAllNetworks marks all networks of a given owner as being not in use.
+func (snr *subnetAllocatorRange) releaseAllNetworks(owner string) {
+	for network, existingOwner := range snr.allocMap {
+		if existingOwner == owner {
+			delete(snr.allocMap, network)
+			snr.used--
+		}
+	}
 }

commit 3051974309de40874bd64e63af0d41e01282d057

fixing hybrid subnet allocator
commit 711237766b25e0728006eacb8dc1478b1ce359bb introduced the issue
where we do not add the Hybrid overlay Cluster subnets to the allocator

fix that issue by adding the HYbrid Overlay Cluster subnets so that they
can be allocated.

go-controller/hybrid-overlay/pkg/controller/master.go
@@ -108,7 +108,7 @@ func NewMaster(kube kube.Interface,
 
 	// Add our hybrid overlay CIDRs to the subnetallocator
 	klog.Infof("Allocating subnets")
-	if err := m.allocator.InitRanges(config.Default.ClusterSubnets); err != nil {
+	if err := m.allocator.InitRanges(config.HybridOverlay.ClusterSubnets); err != nil {
 		klog.Errorf("Failed to initialize host subnet allocator ranges: %v", err)
 		return nil, err
 	}