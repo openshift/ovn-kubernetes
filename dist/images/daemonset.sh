2f9b0113af76444850729027a55ccc150ce489a3
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2023-03-02 14:08:48 +0100

    Add flag to enable the feature
    This commit adds a new flag called
    `admin-network-policy-enable` which can be used
    to feature gate Admin Network Policy feature.
    This will be enabled by default on KIND clusters.
    If that is not desired then we might want to
    expose this option also via CLI in kind to help
    users turn it off. But as of now, like other
    features this knob is not user facing.
    
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

--- dist/images/daemonset.sh
+++ dist/images/daemonset.sh
@@ -61,6 +61,7 @@ OVN_DISABLE_FORWARDING=""
 OVN_DISABLE_PKT_MTU_CHECK=""
 OVN_EMPTY_LB_EVENTS=""
 OVN_MULTICAST_ENABLE=""
+OVN_ADMIN_NETWORK_POLICY_ENABLE=""
 OVN_EGRESSIP_ENABLE=
 OVN_EGRESSIP_HEALTHCHECK_PORT=
 OVN_EGRESSFIREWALL_ENABLE=
@@ -225,6 +226,9 @@ while [ "$1" != "" ]; do
   --multicast-enabled)
     OVN_MULTICAST_ENABLE=$VALUE
     ;;
+  --admin-network-policy-enable)
+    OVN_ADMIN_NETWORK_POLICY_ENABLE=$VALUE
+    ;;
   --egress-ip-enable)
     OVN_EGRESSIP_ENABLE=$VALUE
     ;;
@@ -373,6 +377,8 @@ ovn_acl_logging_rate_limit=${OVN_ACL_LOGGING_RATE_LIMIT:-"20"}
 echo "ovn_acl_logging_rate_limit: ${ovn_acl_logging_rate_limit}"
 ovn_hybrid_overlay_enable=${OVN_HYBRID_OVERLAY_ENABLE}
 echo "ovn_hybrid_overlay_enable: ${ovn_hybrid_overlay_enable}"
+ovn_admin_network_policy_enable=${OVN_ADMIN_NETWORK_POLICY_ENABLE}
+echo "ovn_admin_network_policy_enable: ${ovn_admin_network_policy_enable}"
 ovn_egress_ip_enable=${OVN_EGRESSIP_ENABLE}
 echo "ovn_egress_ip_enable: ${ovn_egress_ip_enable}"
 ovn_egress_ip_healthcheck_port=${OVN_EGRESSIP_HEALTHCHECK_PORT}
@@ -482,6 +488,7 @@ ovn_image=${ovnkube_image} \
   ovn_v4_join_subnet=${ovn_v4_join_subnet} \
   ovn_v6_join_subnet=${ovn_v6_join_subnet} \
   ovn_multicast_enable=${ovn_multicast_enable} \
+  ovn_admin_network_policy_enable=${ovn_admin_network_policy_enable}\
   ovn_egress_ip_enable=${ovn_egress_ip_enable} \
   ovn_egress_ip_healthcheck_port=${ovn_egress_ip_healthcheck_port} \
   ovn_multi_network_enable=${ovn_multi_network_enable} \
@@ -530,6 +537,7 @@ ovn_image=${image} \
   ovn_v4_join_subnet=${ovn_v4_join_subnet} \
   ovn_v6_join_subnet=${ovn_v6_join_subnet} \
   ovn_multicast_enable=${ovn_multicast_enable} \
+  ovn_admin_network_policy_enable=${ovn_admin_network_policy_enable}\
   ovn_egress_ip_enable=${ovn_egress_ip_enable} \
   ovn_egress_ip_healthcheck_port=${ovn_egress_ip_healthcheck_port} \
   ovn_egress_service_enable=${ovn_egress_service_enable} \
@@ -563,6 +571,7 @@ ovn_image=${ovnkube_image} \
   ovn_v4_join_subnet=${ovn_v4_join_subnet} \
   ovn_v6_join_subnet=${ovn_v6_join_subnet} \
   ovn_multicast_enable=${ovn_multicast_enable} \
+  ovn_admin_network_policy_enable=${ovn_admin_network_policy_enable}\
   ovn_egress_ip_enable=${ovn_egress_ip_enable} \
   ovn_egress_ip_healthcheck_port=${ovn_egress_ip_healthcheck_port} \
   ovn_egress_firewall_enable=${ovn_egress_firewall_enable} \
