59573a5fdbd7a70d25d2b2783059484141656b1d
Riccardo Ravaioli <rravaiol@redhat.com>
2023-03-06 16:22:37 +0100

    Node healthz server: return unhealthy when pod is to be deleted
    In the node healthz server, leveraged by cloud load balancers to check the node health for services with ExternalTrafficPolicy=Cluster, respond negatively to health checks as soon as the ovnk node pod is to be deleted, by inspecting the pod DeletionTimestamp. This allows cloud load balancers to have a little more time to update their list of healthy nodes before the node actually stops altogether forwarding traffic.
    
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>
    (cherry picked from commit 96d687c579a11d7a84be3da9eeab7fbf6db31ce3)

--- go-controller/pkg/node/healthcheck_node.go
+++ go-controller/pkg/node/healthcheck_node.go
@@ -1,62 +1,102 @@
 package node
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/pkg/errors"
 
 	kapi "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ktypes "k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/clock"
 )
 
+var updateInterval time.Duration = 500 * time.Millisecond
+
 type proxierHealthUpdater struct {
-	address  string
-	nodeRef  *kapi.ObjectReference
-	recorder record.EventRecorder
-	c        clock.Clock
-	healthy  bool
+	address     string
+	nodeRef     *kapi.ObjectReference
+	recorder    record.EventRecorder
+	c           clock.Clock
+	healthy     bool
+	lastUpdated time.Time
+	kubeClient  kubernetes.Interface
 }
 
 // newNodeProxyHealthzServer creates and returns a new proxier health server
 // if the HealthzBindAddress configuration is set. Cloud load balancers use this
 // health check to determine if the node is available for services with ClusterIP
 // traffic policy.
-func newNodeProxyHealthzServer(nodeName, address string, eventRecorder record.EventRecorder) *proxierHealthUpdater {
+func newNodeProxyHealthzServer(nodeName, address string, eventRecorder record.EventRecorder, kubeClient kubernetes.Interface) *proxierHealthUpdater {
 	return &proxierHealthUpdater{
-		address:  address,
-		recorder: eventRecorder,
-		c:        clock.RealClock{},
-		healthy:  true,
+		address:     address,
+		recorder:    eventRecorder,
+		c:           clock.RealClock{},
+		healthy:     true,
+		lastUpdated: time.Time{},
 		nodeRef: &kapi.ObjectReference{
 			Kind:      "Node",
 			Name:      nodeName,
 			UID:       ktypes.UID(nodeName),
 			Namespace: "",
 		},
+		kubeClient: kubeClient,
 	}
+}
+
+func (phu *proxierHealthUpdater) isOvnkNodePodTerminating() bool {
+	namespace := config.Kubernetes.OVNConfigNamespace
+	podName := os.Getenv("POD_NAME")
+	if len(podName) == 0 {
+		klog.Errorf("Found empty env variable POD_NAME")
+		return false
+	}
+
+	pod, err := phu.kubeClient.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+	if err != nil {
+		klog.Errorf("Could not retrieve pod %s/%s: %v", namespace, pod, err)
+		return false
+	}
+
+	return pod.DeletionTimestamp != nil
+}
+
+// isOvnkNodePodHealthy runs isOvnkNodePodTerminating at most every 500 ms and returns true
+// if the ovnkube node pod is not set for deletion.
+func (phu *proxierHealthUpdater) isOvnkNodePodHealthy(now time.Time) bool {
+	if phu.lastUpdated != (time.Time{}) && now.Sub(phu.lastUpdated) < updateInterval {
+		return phu.healthy
+	}
+	isTerminating := phu.isOvnkNodePodTerminating()
+	phu.lastUpdated = now
+	phu.healthy = !isTerminating
+	return phu.healthy
 }
 
 func (phu *proxierHealthUpdater) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
 	resp.Header().Set("X-Content-Type-Options", "nosniff")
-	if phu.healthy {
+	now := phu.c.Now()
+	if phu.isOvnkNodePodHealthy(now) {
 		resp.WriteHeader(http.StatusOK)
 	} else {
 		resp.WriteHeader(http.StatusServiceUnavailable)
 	}
-	now := phu.c.Now()
-	fmt.Fprintf(resp, `{"lastUpdated": %q,"currentTime": %q}`, now, now)
+
+	fmt.Fprintf(resp, `{"lastUpdated": %q,"currentTime": %q}`, phu.lastUpdated, now)
 }
 
 // serveNodeProxyHealthz initializes and runs the healthz server. It will always
 // report healthy while the node process is running.
-// TODO: connect node health to something useful
 func (phu *proxierHealthUpdater) Start(stopChan chan struct{}, wg *sync.WaitGroup) {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/healthz", phu)
