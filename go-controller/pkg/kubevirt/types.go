831d2de869a7f9967738061d7b9976a9bfdb3218
Enrique Llorente <ellorent@redhat.com>
2023-03-08 16:59:38 +0100

    pod, controller: save/load switch name from pod
    At live migration the IP has to follow the VM and at kubevirt it means
    that the new virt-launcher pod has to re-use the address from the old
    virt-launcher. This change annotate the virt-launcher pods with the
    switch owning the subnet so it's possible to query the IPAM system with
    the proper switch after live migration
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/pkg/kubevirt/types.go
+++ go-controller/pkg/kubevirt/types.go
@@ -2,4 +2,16 @@ package kubevirt
 
 const (
 	AllowPodBridgeNetworkLiveMigrationAnnotation = "kubevirt.io/allow-pod-bridge-network-live-migration"
+	OriginalSwitchNameLabel                      = "k8s.ovn.org/original-switch-name"
+	VMLabel                                      = "kubevirt.io/vm"
 )
+
+// NetworkInfo is the network information common to all the pods involve
+// on the same vm
+type NetworkInfo struct {
+	// OriginalSwitchName is the switch name where the vm was created
+	OriginalSwitchName string
+
+	// Status is the OVN network annotations
+	Status string
+}
