8d4fd0f78e59b6b22a5994eff4a366851e5cd934
Riccardo Ravaioli <rravaiol@redhat.com>
2023-03-15 10:53:30 +0100

    Cleanup of endpoint handling functions
    hasLocalHostNetworkEndpoints and other endpointslice-related functions were inside healthcheck_service.go (and previously, healthcheck.go), but they are actually never used by service health checks. They are now all in util/kube.go, they reuse common code and are tested against in unit tests.
    
    Little cleanup of the code calling these functions from service/endpointslice add/update/delete.
    
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>
    (cherry picked from commit 8c7ea95849bb7abc05da1f06297af7a55f4e5204)
    (cherry picked from commit 8e24ec64c0f108fb0b2ee200feced811505dcf98)

--- go-controller/pkg/node/gateway_localnet_linux_test.go
+++ go-controller/pkg/node/gateway_localnet_linux_test.go
@@ -2227,6 +2227,7 @@ var _ = Describe("Node Operations", func() {
 
 				ep1 := discovery.Endpoint{
 					Addresses: []string{"192.168.18.15"}, // host-networked endpoint local to this node
+					NodeName:  &fakeNodeName,
 				}
 				epPort1 := discovery.EndpointPort{
 					Name: &epPortName,
@@ -2518,6 +2519,7 @@ var _ = Describe("Node Operations", func() {
 				)
 				ep1 := discovery.Endpoint{
 					Addresses: []string{"192.168.18.15"}, // host-networked endpoint local to this node
+					NodeName:  &fakeNodeName,
 				}
 				epPort1 := discovery.EndpointPort{
 					Name: &epPortName,
@@ -2692,6 +2694,7 @@ var _ = Describe("Node Operations", func() {
 				// host-networked endpoint, should not have an SNAT rule created
 				ep2 := discovery.Endpoint{
 					Addresses: []string{"192.168.18.15"},
+					NodeName:  &fakeNodeName,
 				}
 				// endpointSlice.Endpoints is ovn-networked so this will
 				// come under !hasLocalHostNetEp case
