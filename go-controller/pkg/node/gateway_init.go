d81915ab75c92f5d8e58afcad0d3f8bdd97575ac
Martin Kennelly <mkennell@redhat.com>
2023-11-11 09:54:49 +0000

    Refactor route manager and fix panic
    Commit best not viewed by diff but entire file.
    
    After upgrading the netlink lib, remove the 'any'
    route hacks which compensated for an any route
    being represented as nil in the netlink.Route.dst field.
    
    Fix panic in apply route where if no routes exists
    when we apply a route, it will panic.
    
    Remove needless netlink mock infra and use exiting mock infra
    in ovn utils.
    
    Remove poor and needless abstraction (RoutesPerLink), because
    it adds needless complexity and doesnt offer any benefit over
    simply using netlink.Route type.
    
    Change store key to link index instead of name. A links name
    can change, but its link index will not over the life time of a
    link.
    
    Dont use netlinks.Routes method Equal to compare routes.
    A user fills a limit subset of the Route fields and we save this
    in store. If we retreive that applied route from netlink, other
    fields are populated and comparisson is more difficult.
    Also, the Equal method doesnt compare if the MTU in a route is
    equal and we care about this field.
    
    Add default route unit tests.
    
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- go-controller/pkg/node/gateway_init.go
+++ go-controller/pkg/node/gateway_init.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strings"
 
+	"github.com/vishvananda/netlink"
 	"k8s.io/klog/v2"
 	utilnet "k8s.io/utils/net"
 
@@ -287,7 +288,6 @@ func configureSvcRouteViaInterface(routeManager *routemanager.Controller, iface 
 		return fmt.Errorf("unable to get link for %s, error: %v", iface, err)
 	}
 
-	var routes []routemanager.Route
 	for _, subnet := range config.Kubernetes.ServiceCIDRs {
 		gwIP, err := util.MatchIPFamily(utilnet.IsIPv6CIDR(subnet), gwIPs)
 		if err != nil {
@@ -301,15 +301,7 @@ func configureSvcRouteViaInterface(routeManager *routemanager.Controller, iface 
 		}
 		subnetCopy := *subnet
 		gwIPCopy := gwIP[0]
-		routes = append(routes, routemanager.Route{
-			GwIP:   gwIPCopy,
-			Subnet: &subnetCopy,
-			MTU:    mtu,
-			SrcIP:  nil,
-		})
-	}
-	if len(routes) > 0 {
-		routeManager.Add(routemanager.RoutesPerLink{Link: link, Routes: routes})
+		routeManager.Add(netlink.Route{LinkIndex: link.Attrs().Index, Gw: gwIPCopy, Dst: &subnetCopy, MTU: mtu})
 	}
 	return nil
 }
