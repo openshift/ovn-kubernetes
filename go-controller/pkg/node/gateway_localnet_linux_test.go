ae07204bf169923ad55f1ec67efc16e439d6cb68
Tim Rozet <trozet@redhat.com>
2024-03-28 17:17:42 -0400

    Fixes inefficient service parsing
    For every service, service port, and loadbalancer or external IP, we
    would generate ARP flows. This function, would list all ports from OVS.
    That call takes 4ms. Therefore if I have 1000 external IPs on a service
    with a single port, that would cause 4 seconds of delay.
    
    This commit optimizes it to only call it once per service update.
    
    Conflicts in both files:
    gateway_shared_intf - minor conflicts where host zone was not yet a
    config pkg variable, and one openflow in 4.15 does not have dl_src
    
    gateway_localnet_linux_test - only expects one ovs show command
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit d8f7288442129216dcffe21cf22a7c67bbc854c9)
    (cherry picked from commit 332072c35654e968cacf259094715c6ad38c75e7)
    (cherry picked from commit 178f08965b2bc461644f96da1b250522ade744e0)

--- go-controller/pkg/node/gateway_localnet_linux_test.go
+++ go-controller/pkg/node/gateway_localnet_linux_test.go
@@ -632,9 +632,6 @@ var _ = Describe("Node Operations", func() {
 				fakeOvnNode.fakeExec.AddFakeCmd(&ovntest.ExpectedCmd{
 					Cmd: "ovs-ofctl show ",
 				})
-				fakeOvnNode.fakeExec.AddFakeCmd(&ovntest.ExpectedCmd{
-					Cmd: "ovs-ofctl show ",
-				})
 				service := *newService("service1", "namespace1", "10.129.0.2",
 					[]v1.ServicePort{
 						{
@@ -1316,9 +1313,6 @@ var _ = Describe("Node Operations", func() {
 				fakeOvnNode.fakeExec.AddFakeCmd(&ovntest.ExpectedCmd{
 					Cmd: "ovs-ofctl show ",
 				})
-				fakeOvnNode.fakeExec.AddFakeCmd(&ovntest.ExpectedCmd{
-					Cmd: "ovs-ofctl show ",
-				})
 
 				service := *newService("service1", "namespace1", clusterIPv4,
 					[]v1.ServicePort{
@@ -1770,7 +1764,6 @@ var _ = Describe("Node Operations", func() {
 				key, err := retry.GetResourceKey(&service)
 				Expect(err).NotTo(HaveOccurred())
 				retry.CheckRetryObjectEventually(key, true, nodePortWatcherRetry)
-
 				// check iptables
 				f4 := iptV4.(*util.FakeIPTables)
 				err = f4.MatchState(expectedTables)
