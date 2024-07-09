b38199d7a795f0eea541eac8eed695c4b2518cc1
Nadia Pinaeva <n.m.pinaeva@gmail.com>
2024-07-09 13:28:52 +0200

    Make OVSDBTimeout for transactions a part of config.Global instead of const. A reasonable value for this timeout depends on the size of the cluster and making it configurable may help solve crashloop problems. It also speeds up testing as timeout may be set to lower value for unit tests. Set default value to 100 second, to allow larger transactions, e.g. 50 address set batch updated on address set sync with dbIDs.
    Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>
    (cherry picked from commit 8e3c8c2c156e983a2165ff9f48fb23a35571bed6)
    (cherry picked from commit 92dcc408f6e1bd199e688037f45f2882105581f8)
    (cherry picked from commit de57139b404a1a80f011854384b0c1235c866502)

--- go-controller/pkg/ovn/pods_test.go
+++ go-controller/pkg/ovn/pods_test.go
@@ -505,7 +505,7 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 					},
 				)
 
-				ctext, cancel := context.WithTimeout(context.Background(), ovntypes.OVSDBTimeout)
+				ctext, cancel := context.WithTimeout(context.Background(), config.Default.OVSDBTxnTimeout)
 				defer cancel()
 				lsl := []nbdb.LogicalSwitch{}
 				err := fakeOvn.controller.nbClient.WhereCache(
@@ -1061,11 +1061,11 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 				// sleep long enough for TransactWithRetry to fail, causing pod add to fail
-				time.Sleep(ovntypes.OVSDBTimeout + time.Second)
+				time.Sleep(config.Default.OVSDBTxnTimeout + time.Second)
 
 				// check to see if the pod retry cache has an entry for this policy
 				retry.CheckRetryObjectEventually(key, true, fakeOvn.controller.retryPods)
-				connCtx, cancel := context.WithTimeout(context.Background(), ovntypes.OVSDBTimeout)
+				connCtx, cancel := context.WithTimeout(context.Background(), config.Default.OVSDBTxnTimeout)
 
 				defer cancel()
 				resetNBClient(connCtx, fakeOvn.controller.nbClient)
@@ -1141,11 +1141,11 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 				// sleep long enough for TransactWithRetry to fail, causing pod delete to fail
-				time.Sleep(ovntypes.OVSDBTimeout + time.Second)
+				time.Sleep(config.Default.OVSDBTxnTimeout + time.Second)
 
 				// check to see if the pod retry cache has an entry for this pod
 				retry.CheckRetryObjectEventually(key, true, fakeOvn.controller.retryPods)
-				connCtx, cancel := context.WithTimeout(context.Background(), ovntypes.OVSDBTimeout)
+				connCtx, cancel := context.WithTimeout(context.Background(), config.Default.OVSDBTxnTimeout)
 
 				defer cancel()
 				resetNBClient(connCtx, fakeOvn.controller.nbClient)
@@ -1217,7 +1217,7 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 				err = fakeOvn.controller.WatchPods()
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
 				// sleep long enough for TransactWithRetry to fail, causing pod add to fail
-				time.Sleep(ovntypes.OVSDBTimeout + time.Second)
+				time.Sleep(config.Default.OVSDBTxnTimeout + time.Second)
 
 				// wait until retry entry appears
 
@@ -1251,7 +1251,7 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 
 				// restore nbdb, trigger a retry and verify that the retry entry gets deleted
 				// because it reached retry.MaxFailedAttempts and the corresponding pod has NOT been added to OVN
-				connCtx, cancel := context.WithTimeout(context.Background(), ovntypes.OVSDBTimeout)
+				connCtx, cancel := context.WithTimeout(context.Background(), config.Default.OVSDBTxnTimeout)
 				defer cancel()
 				resetNBClient(connCtx, fakeOvn.controller.nbClient)
 
@@ -1335,7 +1335,7 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 				// sleep long enough for TransactWithRetry to fail, causing pod delete to fail
-				time.Sleep(ovntypes.OVSDBTimeout + time.Second)
+				time.Sleep(config.Default.OVSDBTxnTimeout + time.Second)
 
 				// wait until retry entry appears and check that it is marked for deletion
 				retry.CheckRetryObjectMultipleFieldsEventually(
@@ -1364,7 +1364,7 @@ var _ = ginkgo.Describe("OVN Pod Operations", func() {
 
 				// restore nbdb and verify that the retry entry gets deleted because it reached
 				// retry.MaxFailedAttempts and the corresponding pod has NOT been deleted from OVN
-				connCtx, cancel := context.WithTimeout(context.Background(), ovntypes.OVSDBTimeout)
+				connCtx, cancel := context.WithTimeout(context.Background(), config.Default.OVSDBTxnTimeout)
 				defer cancel()
 				resetNBClient(connCtx, fakeOvn.controller.nbClient)
 
