6f94d86317e36eaa9f597604fd03f44271d1abe0
Peng Liu <pliu@redhat.com>
2024-08-05 03:36:24 +0000

    Use more exact name match when deleting static routes to HO nodes.
    It can help to avoid the unintended match when the '-gr' substring
    exists in a node name.
    
    Signed-off-by: Peng Liu <pliu@redhat.com>

--- go-controller/pkg/ovn/hybrid_test.go
+++ go-controller/pkg/ovn/hybrid_test.go
@@ -1357,7 +1357,7 @@ var _ = ginkgo.Describe("Hybrid SDN Master Operations", func() {
 			gomega.Eventually(func() ([]*nbdb.LogicalRouterStaticRoute, error) {
 				p := func(item *nbdb.LogicalRouterStaticRoute) bool {
 					if item.ExternalIDs["name"] == "hybrid-subnet-node1-gr" ||
-						strings.Contains(item.ExternalIDs["name"], "hybrid-subnet-node1") {
+						item.ExternalIDs["name"] == "hybrid-subnet-node1:node-windows" {
 						return true
 					}
 					return false
