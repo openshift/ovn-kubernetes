3d9c8092d0679e36bcbe1af345e2dba74ed7c302
Or Mergi <ormergi@redhat.com>
2025-03-03 21:12:03 +0200

    test,e2e: CUDN CRD CEL validation tests
    Introduce tests for CUDN CRD  CEL validations, consist of two tests:
    1. Positive test verifies various valid CRs created successfully.
       Apply the YAMLs defined in "test/e2e/testdata/cudn"
       Expect no error to have occurred.
    2. Negative test verifies invalid CRs get rejected by the API server.
       Apply the YAMLs defined in "test/e2e/testdata/cudn"
       Expect failure with the expected error.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

---
+++ test/e2e/testdata/cudn/valid-scenarios-localnet.go
@@ -0,0 +1,80 @@
+package cudn
+
+import "github.com/ovn-org/ovn-kubernetes/test/e2e/testdata"
+
+var LocalnetValid = []testdata.ValidateCRScenario{
+	{
+		Description: "should create localnet topology successfully - minimal",
+		Manifest: `
+---
+apiVersion: k8s.ovn.org/v1
+kind: ClusterUserDefinedNetwork
+metadata:
+  name: localnet-minimal-success
+spec:
+  namespaceSelector:
+    matchLabels:
+      kubernetes.io/metadata.name: red
+  network:
+    topology: Localnet
+    localnet:
+      role: Secondary
+      physicalNetworkName: test
+      subnets: ["10.0.0.0/24"]
+`,
+	},
+	{
+		Description: "should create localnet topology successfully - ipam persistent",
+		Manifest: `
+---
+apiVersion: k8s.ovn.org/v1
+kind: ClusterUserDefinedNetwork
+metadata:
+  name: localnet-ipam-lifecycle-persistent-success
+spec:
+  namespaceSelector:
+    matchExpressions:
+      - key: kubernetes.io/metadata.name
+        operator: In
+        values: ["red", "blue"]
+  network:
+    topology: Localnet
+    localnet:
+      role: Secondary
+      physicalNetworkName: test
+      ipam: {lifecycle: Persistent}
+      subnets: ["192.168.0.0/16", "2001:dbb::/64"]
+      excludeSubnets: ["192.168.0.1/32", "2001:dbb::1/128"]
+      vlan: 
+        mode: Access
+        access: {id: 3}
+      mtu: 9000
+`,
+	},
+	{
+		Description: "should create localnet topology successfully - ipam disabled",
+		Manifest: `
+---
+apiVersion: k8s.ovn.org/v1
+kind: ClusterUserDefinedNetwork
+metadata:
+  name: localnet-ipam-disabled-success
+spec:
+  namespaceSelector:
+    matchExpressions:
+      - key: kubernetes.io/metadata.name
+        operator: In
+        values: ["red", "blue"]
+  network:
+    topology: Localnet
+    localnet:
+      role: Secondary
+      physicalNetworkName: test
+      ipam: {mode: Disabled}
+      vlan: 
+        mode: Access
+        access: {id: 4094}
+      mtu: 9000
+`,
+	},
+}
