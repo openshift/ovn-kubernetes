a1d47314593388f86851b32eacbde99f8cc069a0
Or Mergi <ormergi@redhat.com>
2025-05-15 18:01:39 +0300

    contrib,kind: Use skopeo to get the actual ovnkube-image digest
    When working with local registry, the automation inspect the built
    ovnkube-image digest (SHA) and pass it to the daemonset manifest,
    in order to ensure the latest built image is deployed.
    
    Some container runtime may not retain the same digest, result in having
    one image digest in the local runtime image and different one on the
    local registry.
    
    To avoid that and get the actual image digest that exist in the local
    registry, use skopeo to inspect the image and get the actual digest.
    
    This change introduce new dependency for the project.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- contrib/kind.sh
+++ contrib/kind.sh
@@ -504,6 +504,11 @@ check_dependencies() {
   	  echo "Dependency not met: Neither docker nor podman found"
   	  exit 1
   fi
+
+  if command_exists podman && ! command_exists skopeo; then
+    echo "Dependency not met: skopeo not installed. Run the following command to install it: 'sudo dnf install skopeo'"
+    exit 1
+  fi
 }
 
 OPENSSL=""
@@ -854,8 +859,14 @@ build_ovn_image() {
 create_ovn_kube_manifests() {
     local ovnkube_image=${OVN_IMAGE}
     if [ "$KIND_LOCAL_REGISTRY" == true ];then
-      # When updating with local registry we have to reference the sha
-      ovnkube_image=$($OCI_BIN inspect --format='{{index .RepoDigests 0}}' $OVN_IMAGE)
+      # When updating with local registry we have to reference the image digest (SHA)
+      # Check the image digest in the local registry because it might be different then the digest in the local container runtime
+      if [ "$OCI_BIN" == "podman" ]; then
+        # due to differences how podman and docker persist images, for podman use skopeo to get the image and digest.
+        ovnkube_image=$(skopeo inspect --format "{{.Name}}@{{.Digest}}" --tls-verify=false  "docker://$OVN_IMAGE")
+      else
+        ovnkube_image=$($OCI_BIN inspect --format='{{index .RepoDigests 0}}' $OVN_IMAGE)
+      fi
     fi
     pushd ${DIR}/../dist/images
     if [ "$OVN_ENABLE_INTERCONNECT" == true ]; then
