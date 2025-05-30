080e3213afb0df505358979c80a0350e4448b6dc
Jamo Luhrsen <jluhrsen@gmail.com>
2025-05-30 14:07:45 -0700

    Revert "Merge remote-tracking branch 'upstream/master' into merge-20250416"
    This reverts commit 9a73d1fbd2735b9bdbaa4da1dd3d94ede94f9646, reversing
    changes made to 3b45a034f51363d473b82d3e44f76f285cc530b3.

--- test/e2e/kubevirt/types.go
+++ test/e2e/kubevirt/types.go
@@ -1,8 +1,7 @@
 package kubevirt
 
 const (
-	FedoraCoreOSContainerDiskImage          = "quay.io/kubevirtci/fedora-coreos-kubevirt:v20230905-be4fa50"
-	FedoraWithTestToolingContainerDiskImage = "quay.io/kubevirtci/fedora-with-test-tooling:v20241128-4d4c8fe"
-	FedoraContainerDiskImage                = "quay.io/containerdisks/fedora:39"
-	FakeLauncherImage                       = "quay.io/nmstate/c10s-nmstate-dev:latest"
+	FedoraCoreOSContainerDiskImage = "quay.io/kubevirtci/fedora-coreos-kubevirt:v20230905-be4fa50"
+	FedoraContainerDiskImage       = "quay.io/kubevirtci/fedora-with-test-tooling:v20241128-4d4c8fe"
+	FakeLauncherImage              = "quay.io/nmstate/c10s-nmstate-dev:latest"
 )
--- test/e2e/testdata/scenario.go
+++
@@ -1,8 +0,0 @@
-package testdata
-
-// ValidateCRScenario represent test scenario where a manifest is applied and failed with the expected error
-type ValidateCRScenario struct {
-	Description string
-	Manifest    string
-	ExpectedErr string
-}
