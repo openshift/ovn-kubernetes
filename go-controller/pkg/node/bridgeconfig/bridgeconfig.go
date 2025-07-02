29a4866c47af269d94191045e94c5752135d7364
Nadia Pinaeva <n.m.pinaeva@gmail.com>
2025-07-02 14:43:41 +0200

    [bridgeconfig] start moving methods that use internal mutex to the pkg
    Update gatewayReady function to only return bool as it always returns
    nil error.
    
    Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>

--- go-controller/pkg/node/bridgeconfig/bridgeconfig.go
+++ go-controller/pkg/node/bridgeconfig/bridgeconfig.go
@@ -345,6 +345,30 @@ func (b *BridgeConfiguration) PatchedNetConfigs() []*BridgeUDNConfiguration {
 	return result
 }
 
+// IsGatewayReady checks if patch ports of every netConfig are present.
+// used by gateway on newGateway readyFunc
+func (b *BridgeConfiguration) IsGatewayReady() bool {
+	b.Mutex.Lock()
+	defer b.Mutex.Unlock()
+	for _, netConfig := range b.NetConfig {
+		ready := gatewayReady(netConfig.PatchPort)
+		if !ready {
+			return false
+		}
+	}
+	return true
+}
+
+func gatewayReady(patchPort string) bool {
+	// Get ofport of patchPort
+	ofport, _, err := util.GetOVSOfPort("--if-exists", "get", "interface", patchPort, "ofport")
+	if err != nil || len(ofport) == 0 {
+		return false
+	}
+	klog.Info("Gateway is ready")
+	return true
+}
+
 func getIntfName(gatewayIntf string) (string, error) {
 	// The given (or autodetected) interface is an OVS bridge and this could be
 	// created by us using util.NicToBridge() or it was pre-created by the user.
