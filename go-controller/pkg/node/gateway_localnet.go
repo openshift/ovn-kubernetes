e9cafcd5c2156654b892429b727db970247c5928
Riccardo Ravaioli <rravaiol@redhat.com>
2025-07-15 20:40:32 +0200

    Pass hostSubnets to commonFlows to fix default network -> localnet
    We need to pass the host subnets when adding the flows at priority 102 that allow connections from pods in the default network to pods in a localnet attached to breth0 on the same node. We were previously passing the pod subnets for the node, which is not correct.
    
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>

--- go-controller/pkg/node/gateway_localnet.go
+++ go-controller/pkg/node/gateway_localnet.go
@@ -129,16 +129,16 @@ func newLocalGateway(nodeName string, subnets []*net.IPNet, gwNextHops []net.IP,
 			return fmt.Errorf("failed to update masquerade subnet annotation on node: %s, error: %v", nodeName, err)
 		}
 
-		nodeIPs, _ := gw.nodeIPManager.ListAddresses()
-		gw.openflowManager, err = newGatewayOpenFlowManager(gwBridge, exGwBridge, hostSubnets, nodeIPs)
+		hostIPs, hostSubnets := gw.nodeIPManager.ListAddresses()
+		gw.openflowManager, err = newGatewayOpenFlowManager(gwBridge, exGwBridge, subnets, hostSubnets, hostIPs)
 		if err != nil {
 			return err
 		}
 		// resync flows on IP change
 		gw.nodeIPManager.OnChanged = func() {
 			klog.V(5).Info("Node addresses changed, re-syncing bridge flows")
-			nodeIPs, _ := gw.nodeIPManager.ListAddresses()
-			if err := gw.openflowManager.updateBridgeFlowCache(hostSubnets, nodeIPs); err != nil {
+			hostIPs, hostSubnets := gw.nodeIPManager.ListAddresses()
+			if err := gw.openflowManager.updateBridgeFlowCache(subnets, hostSubnets, hostIPs); err != nil {
 				// very unlikely - somehow node has lost its IP address
 				klog.Errorf("Failed to re-generate gateway flows after address change: %v", err)
 			}
