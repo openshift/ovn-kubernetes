b928217647a7a6eb1cd8787f2357aef497cc54e8
Or Mergi <ormergi@redhat.com>
2025-08-18 21:12:51 +0300

    clustermanager,allocator: Init network pool with infra MACs
    When EnablePreconfiguredUDNAddresses is enabled, initialize the
    pool for primary Layer2 UDNs, with MAC addresses of the network
    GW and management ports to prevent conflicts with pods.
    
    The network logical switch has GW (.1) and management (.2) ports.
    Their MAC address is generated from the IP address.
    Calculate the GW and management MAC addresses from their IP addresses.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- go-controller/pkg/allocator/pod/addr_pool.go
+++ go-controller/pkg/allocator/pod/addr_pool.go
@@ -2,11 +2,15 @@ package pod
 
 import (
 	"fmt"
-
-	corev1 "k8s.io/api/core/v1"
+	"net"
 
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	utilnet "k8s.io/utils/net"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
 
@@ -22,4 +26,34 @@ func GetPoolAddressOwner(pod *corev1.Pod, netInfo util.NetInfo) string {
 
 	// Default to pod-based identifier
 	return fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
+}
+
+// InitializeAddressPool initializes the address pool for the network with allocated MAC addresses.
+// In case an error occur during the process, error is recorded and the process continues to next pod/namespace.
+func (allocator *PodAnnotationAllocator) InitializeAddressPool(eventRec record.EventRecorder) error {
+	networkName := allocator.netInfo.GetNetworkName()
+	infaMACs := calculateSubnetsInfraMACAddresses(allocator.netInfo.Subnets())
+	for owner, mac := range infaMACs {
+		allocator.addressPool.AddMACToPool(networkName, mac, owner)
+	}
+
+	return nil
+}
+
+// calculateSubnetsInfraMACAddresses return map of the network infrastructure mac addresses and owner name.
+// It calculates the gateway (.2) and management (.1) ports MAC addresses from their IP address.
+func calculateSubnetsInfraMACAddresses(subnets []config.CIDRNetworkEntry) map[string]net.HardwareAddr {
+	reservedMACs := map[string]net.HardwareAddr{}
+	for _, subnet := range subnets {
+		if subnet.CIDR != nil && utilnet.IsIPv4CIDR(subnet.CIDR) {
+			gwIP := util.GetNodeGatewayIfAddr(subnet.CIDR)
+			gwMAC := util.IPAddrToHWAddr(gwIP.IP)
+			reservedMACs["gw"] = gwMAC
+
+			mgmtIP := util.GetNodeManagementIfAddr(subnet.CIDR)
+			mgmtMAC := util.IPAddrToHWAddr(mgmtIP.IP)
+			reservedMACs["mgmt"] = mgmtMAC
+		}
+	}
+	return reservedMACs
 }
