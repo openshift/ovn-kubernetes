1fe7b3bd8b84b4628049c2641ce447c61002eb84
Peng Liu <pliu@redhat.com>
2025-08-27 07:54:20 +0000

    Clear stale conntrack UDP entries for nodePorts
    When an EndpointSlice for a UDP NodePort or loadbalancer type of service
    is updated, stale conntrack entries for removed endpoints must be
    flushed. The existing logic failed to do this correctly if the backend
    pod was on a different node. This patch fixes the issue by flushing
    conntrack entries by filtering the nodePort when the node is not
    hosting the backend pod.
    
    In case that the backend pod was on the same node as the service, this
    issue won't happen. Since all old pod entries are removed from the node
    by the function deletePodConntrack when the pod is deleted.
    
    Signed-off-by: Peng Liu <pliu@redhat.com>
    (cherry picked from commit b426934bc27b426d861c3ee0723b7f64fb35a031)
    (cherry picked from commit 46ce09a9e9ad75d388fa9ff678afb814e6c46e89)
    (cherry picked from commit e0a338945fff65418ff4d6da4f24aa6fcd2277cf)
    (cherry picked from commit 5e6ce84668f2d48274cf5d5913b4558c2a1059fc)
    (cherry picked from commit 0d75c796a6678785eb4c030bbee842fa2de4811e)
    Signed-off-by: Venkata Charan Sunku <vsunku@redhat.com>

--- go-controller/pkg/node/default_node_network_controller_test.go
+++ go-controller/pkg/node/default_node_network_controller_test.go
@@ -653,7 +653,6 @@ var _ = Describe("Node", func() {
 			ip       string
 			port     uint16
 			protocol uint8
-			family   netlink.InetFamily
 		}
 
 		// Test data structure for table-driven tests
@@ -666,12 +665,21 @@ var _ = Describe("Node", func() {
 			expectedFilters        []expectedConntrackFilter
 		}
 
-		// Helper to create EndpointSlice
-		makeEndpointSlice := func(portConfigs []struct {
+		type endpointPortConfig struct {
 			name     *string
 			port     int32
 			protocol corev1.Protocol
-		}, addresses []string) *discovery.EndpointSlice {
+		}
+
+		type servicePortConfig struct {
+			name       string
+			port       int32
+			targetPort int32
+			protocol   corev1.Protocol
+		}
+
+		// Helper to create EndpointSlice
+		makeEndpointSlice := func(portConfigs []endpointPortConfig, addresses []string) *discovery.EndpointSlice {
 			ports := make([]discovery.EndpointPort, len(portConfigs))
 			for i, pc := range portConfigs {
 				p := pc.port
@@ -699,12 +707,7 @@ var _ = Describe("Node", func() {
 		}
 
 		// Helper to create Service
-		makeService := func(portConfigs []struct {
-			name       string
-			port       int32
-			targetPort int32
-			protocol   corev1.Protocol
-		}) *corev1.Service {
+		makeService := func(portConfigs []servicePortConfig) *corev1.Service {
 			ports := make([]corev1.ServicePort, len(portConfigs))
 			for i, pc := range portConfigs {
 				ports[i] = corev1.ServicePort{
@@ -724,6 +727,16 @@ var _ = Describe("Node", func() {
 					Ports: ports,
 				},
 			}
+		}
+
+		// Helper to create NodePort or LoadBalancer Service by invoking makeService
+		makeServiceWithNodePort := func(portConfigs []servicePortConfig, nodePorts []int32, svcType corev1.ServiceType) *corev1.Service {
+			svc := makeService(portConfigs)
+			svc.Spec.Type = svcType
+			for i := 0; i < len(nodePorts) && i < len(svc.Spec.Ports); i++ {
+				svc.Spec.Ports[i].NodePort = nodePorts[i]
+			}
+			return svc
 		}
 
 		// Helper function to build expected ConntrackFilter for verification
@@ -826,13 +839,8 @@ var _ = Describe("Node", func() {
 
 			Entry("old endpointslice is nil",
 				reconcileConntrackTestCase{
-					desc: "should not delete any conntrack entries when old endpoint is nil",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:                   "should not delete any conntrack entries when old endpoint is nil",
+					service:                makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice:       nil,
 					newEndpointSlice:       &discovery.EndpointSlice{},
 					expectedConntrackCalls: 0,
@@ -841,69 +849,42 @@ var _ = Describe("Node", func() {
 
 			Entry("service exists with matching unnamed port",
 				reconcileConntrackTestCase{
-					desc: "should delete conntrack with service port for unnamed port",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should delete conntrack with service port for unnamed port",
+					service: makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1"},
 					),
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 1,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
+						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
 
 			Entry("service exists with matching named port",
 				reconcileConntrackTestCase{
-					desc: "should delete conntrack with service port for named port",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "http", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should delete conntrack with service port for named port",
+					service: makeService([]servicePortConfig{{name: "http", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: strPtr("http"), port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: strPtr("http"), port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1"},
 					),
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 1,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
+						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
 
 			Entry("service exists but port name mismatch",
 				reconcileConntrackTestCase{
-					desc: "should skip conntrack deletion when port name doesn't match",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "http", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should skip conntrack deletion when port name doesn't match",
+					service: makeService([]servicePortConfig{{name: "http", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: strPtr("grpc"), port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: strPtr("grpc"), port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1"},
 					),
 					newEndpointSlice:       nil,
@@ -916,11 +897,7 @@ var _ = Describe("Node", func() {
 					desc:    "should return early without deleting conntrack when service not found",
 					service: nil,
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1"},
 					),
 					newEndpointSlice:       nil,
@@ -930,19 +907,10 @@ var _ = Describe("Node", func() {
 
 			Entry("TCP protocol should be skipped",
 				reconcileConntrackTestCase{
-					desc: "should skip conntrack deletion for TCP protocol",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: tcpProtocol}}),
+					desc:    "should skip conntrack deletion for TCP protocol",
+					service: makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: tcpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: tcpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: tcpProtocol}},
 						[]string{"10.0.0.1"},
 					),
 					newEndpointSlice:       nil,
@@ -952,78 +920,51 @@ var _ = Describe("Node", func() {
 
 			Entry("multiple endpoints",
 				reconcileConntrackTestCase{
-					desc: "should delete conntrack for each endpoint",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should delete conntrack for each endpoint",
+					service: makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1", "10.0.0.2", "10.0.0.3"},
 					),
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 3,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
-						{ip: "10.0.0.2", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
-						{ip: "10.0.0.3", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
+						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+						{ip: "10.0.0.2", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+						{ip: "10.0.0.3", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
 
 			Entry("IPv6 endpoint",
 				reconcileConntrackTestCase{
-					desc: "should delete conntrack for IPv6 endpoint",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should delete conntrack for IPv6 endpoint",
+					service: makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"fd00::1"},
 					),
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 1,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "fd00::1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V6},
+						{ip: "fd00::1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
 
 			Entry("dual-stack endpoints",
 				reconcileConntrackTestCase{
-					desc: "should delete conntrack for both IPv4 and IPv6",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
+					desc:    "should delete conntrack for both IPv4 and IPv6",
+					service: makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
+						[]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}},
 						[]string{"10.0.0.1", "fd00::1"},
 					),
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 2,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
-						{ip: "fd00::1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V6},
+						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+						{ip: "fd00::1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
@@ -1031,21 +972,12 @@ var _ = Describe("Node", func() {
 			Entry("multiple service ports with matching names",
 				reconcileConntrackTestCase{
 					desc: "should match correct service port by name for multiple ports",
-					service: makeService([]struct {
-						name       string
-						port       int32
-						targetPort int32
-						protocol   corev1.Protocol
-					}{
+					service: makeService([]servicePortConfig{
 						{name: "http", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol},
 						{name: "https", port: testServicePort2, targetPort: testEndpointPort2, protocol: udpProtocol},
 					}),
 					oldEndpointSlice: makeEndpointSlice(
-						[]struct {
-							name     *string
-							port     int32
-							protocol corev1.Protocol
-						}{
+						[]endpointPortConfig{
 							{name: strPtr("http"), port: testEndpointPort1, protocol: udpProtocol},
 							{name: strPtr("https"), port: testEndpointPort2, protocol: udpProtocol},
 						},
@@ -1054,11 +986,94 @@ var _ = Describe("Node", func() {
 					newEndpointSlice:       nil,
 					expectedConntrackCalls: 2,
 					expectedFilters: []expectedConntrackFilter{
-						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
-						{ip: "10.0.0.1", port: uint16(testServicePort2), protocol: syscall.IPPROTO_UDP, family: netlink.FAMILY_V4},
+						{ip: "10.0.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+						{ip: "10.0.0.1", port: uint16(testServicePort2), protocol: syscall.IPPROTO_UDP},
 					},
 				},
 			),
+			Entry("NodePort service", reconcileConntrackTestCase{
+				desc: "should delete conntrack entries for both service port and NodePort",
+				service: makeServiceWithNodePort([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}},
+					[]int32{30000}, corev1.ServiceTypeNodePort),
+				oldEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.1"}),
+				newEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.2"}),
+				expectedConntrackCalls: 2,
+				expectedFilters: []expectedConntrackFilter{
+					{ip: "10.128.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+					{ip: "10.128.0.1", port: 30000, protocol: syscall.IPPROTO_UDP},
+				},
+			}),
+			Entry("NodePort service with mixed protocols should only clean UDP NodePort", reconcileConntrackTestCase{
+				desc: "should only delete conntrack for UDP NodePort, not TCP (protocol filtering)",
+				service: makeServiceWithNodePort([]servicePortConfig{
+					{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol},
+					{name: "", port: testServicePort2, targetPort: testEndpointPort1, protocol: tcpProtocol},
+				}, []int32{30000, 30001}, corev1.ServiceTypeNodePort),
+				oldEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.1"}),
+				newEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.2"}),
+				expectedConntrackCalls: 2, // Only UDP: service port + NodePort (TCP port 30001 should be skipped)
+				expectedFilters: []expectedConntrackFilter{
+					{ip: "10.128.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+					{ip: "10.128.0.1", port: 30000, protocol: syscall.IPPROTO_UDP},
+				},
+			}),
+			Entry("NodePort service with multiple UDP ports", reconcileConntrackTestCase{
+				desc: "should delete conntrack entries only for the specific NodePort that changed",
+				service: makeServiceWithNodePort([]servicePortConfig{
+					{name: "dns", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol},
+					{name: "snmp", port: testServicePort2, targetPort: testEndpointPort1, protocol: udpProtocol},
+				}, []int32{30000, 30002}, corev1.ServiceTypeNodePort),
+				oldEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: strPtr("dns"), port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.1"}),
+				newEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: strPtr("dns"), port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.2"}),
+				expectedConntrackCalls: 2, // service port + NodePort for "dns" only
+				expectedFilters: []expectedConntrackFilter{
+					{ip: "10.128.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+					{ip: "10.128.0.1", port: 30000, protocol: syscall.IPPROTO_UDP},
+				},
+			}),
+			Entry("LoadBalancer service with NodePort allocation", reconcileConntrackTestCase{
+				desc: "should delete conntrack entries for both service port and NodePort",
+				service: func() *corev1.Service {
+					svc := makeServiceWithNodePort([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}},
+						[]int32{30000}, corev1.ServiceTypeLoadBalancer)
+					svc.Status = corev1.ServiceStatus{
+						LoadBalancer: corev1.LoadBalancerStatus{
+							Ingress: []corev1.LoadBalancerIngress{{IP: "5.5.5.5"}},
+						},
+					}
+					return svc
+				}(),
+				oldEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.1"}),
+				newEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.2"}),
+				expectedConntrackCalls: 2,
+				expectedFilters: []expectedConntrackFilter{
+					{ip: "10.128.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+					{ip: "10.128.0.1", port: 30000, protocol: syscall.IPPROTO_UDP},
+				},
+			}),
+			Entry("LoadBalancer service with AllocateLoadBalancerNodePorts=false", func() reconcileConntrackTestCase {
+				allocateNodePorts := false
+				return reconcileConntrackTestCase{
+					desc: "should only delete conntrack entries for service port (no NodePort)",
+					service: func() *corev1.Service {
+						svc := makeService([]servicePortConfig{{name: "", port: testServicePort1, targetPort: testEndpointPort1, protocol: udpProtocol}})
+						svc.Spec.Type = corev1.ServiceTypeLoadBalancer
+						svc.Spec.AllocateLoadBalancerNodePorts = &allocateNodePorts
+						svc.Status = corev1.ServiceStatus{
+							LoadBalancer: corev1.LoadBalancerStatus{
+								Ingress: []corev1.LoadBalancerIngress{{IP: "5.5.5.5"}},
+							},
+						}
+						return svc
+					}(),
+					oldEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.1"}),
+					newEndpointSlice:       makeEndpointSlice([]endpointPortConfig{{name: nil, port: testEndpointPort1, protocol: udpProtocol}}, []string{"10.128.0.2"}),
+					expectedConntrackCalls: 1,
+					expectedFilters: []expectedConntrackFilter{
+						{ip: "10.128.0.1", port: uint16(testServicePort1), protocol: syscall.IPPROTO_UDP},
+					},
+				}
+			}()),
 		)
 	})
 })
