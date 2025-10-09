2adfb7d7b18ed77c32b1f7238c88c1df66574c5a
Peng Liu <pliu@redhat.com>
2025-10-09 04:01:57 -0400

    Add OpenFlow rules for NodePort in shared gateway mode
    This commit introduces new OpenFlow rules to correctly handle NodePort
    service traffic in a shared gateway configuration.
    
    The new rules address two scenarios:
    
    1.  A rule with priority 109 is added to drop traffic originating from
        OVN towards a NodePort. This prevents ingress traffic from being
        incorrectly forwarded to the host during OVN logical router
        resynchronizations.
    
    2.  A higher-priority rule (110) is added to allow traffic from the
        local host or pods destined for a NodePort service to egress to the
        physical network. This ensures that local clients can access
        services via their NodePort.

--- go-controller/pkg/node/gateway_localnet_linux_test.go
+++ go-controller/pkg/node/gateway_localnet_linux_test.go
@@ -1193,6 +1193,10 @@ var _ = Describe("Node Operations", func() {
 				}
 				expectedNodePortFlows := []string{
 					"cookie=0x453ae29bcbbc08bd, priority=110, in_port=eth0, tcp, tp_dst=31111, actions=output:patch-breth0_ov",
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=109, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, actions=drop",
+						gwMAC),
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, ip, nw_src=%s, actions=ct(commit, zone=64000, exec(set_field:0x1->ct_mark)), output:NORMAL",
+						gwMAC, v4localnetGatewayIP),
 					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_src=31111, actions=output:eth0",
 						gwMAC),
 				}
@@ -2306,6 +2310,10 @@ var _ = Describe("Node Operations", func() {
 				expectedFlows := []string{
 					// default
 					"cookie=0x453ae29bcbbc08bd, priority=110, in_port=eth0, tcp, tp_dst=31111, actions=output:patch-breth0_ov",
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=109, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, actions=drop",
+						gwMAC),
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, ip, nw_src=%s, actions=ct(commit, zone=64000, exec(set_field:0x1->ct_mark)), output:NORMAL",
+						gwMAC, v4localnetGatewayIP),
 					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_src=31111, actions=output:eth0",
 						gwMAC),
 				}
@@ -2596,6 +2604,10 @@ var _ = Describe("Node Operations", func() {
 				expectedFlows := []string{
 					// default
 					"cookie=0x453ae29bcbbc08bd, priority=110, in_port=eth0, tcp, tp_dst=31111, actions=output:patch-breth0_ov",
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=109, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, actions=drop",
+						gwMAC),
+					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_dst=31111, ip, nw_src=%s, actions=ct(commit, zone=64000, exec(set_field:0x1->ct_mark)), output:NORMAL",
+						gwMAC, v4localnetGatewayIP),
 					fmt.Sprintf("cookie=0x453ae29bcbbc08bd, priority=110, in_port=patch-breth0_ov, dl_src=%s, tcp, tp_src=31111, actions=output:eth0",
 						gwMAC),
 				}
