6e4506419b12ae6e3b6e7c740a721ea7c98fc809
Tim Rozet <trozet@nvidia.com>
2025-10-10 22:49:18 -0400

    Add management port cleanup at start up
    Signed-off-by: Tim Rozet <trozet@nvidia.com>

--- go-controller/pkg/node/gateway_udn.go
+++ go-controller/pkg/node/gateway_udn.go
@@ -12,6 +12,7 @@ import (
 	"golang.org/x/sys/unix"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/util/retry"
@@ -20,15 +21,19 @@ import (
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/knftables"
 
+	"github.com/ovn-kubernetes/libovsdb/client"
+
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/generator/udn"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/kube"
+	ovsops "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/libovsdb/ops/ovs"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/node/iprulemanager"
 	nodenft "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/node/nftables"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/node/vrfmanager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 	utilerrors "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util/errors"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/vswitchd"
 )
 
 const (
@@ -473,13 +478,14 @@ func (udng *UserDefinedNetworkGateway) addUDNManagementPortIPs(mpLink netlink.Li
 	return nil
 }
 
-// deleteUDNManagementPort does the following:
-// STEP1: deletes the OVS interface on br-int for the UDN's management port interface
-// STEP2: deletes the mac address from the annotation
+// deleteUDNManagementPort deletes the OVS interface on br-int for the UDN's management port interface
 func (udng *UserDefinedNetworkGateway) deleteUDNManagementPort() error {
-	var err error
 	interfaceName := util.GetNetworkScopedK8sMgmtHostIntfName(uint(udng.GetNetworkID()))
-	// STEP1
+	klog.V(3).Infof("Removing OVS management port interface %s for network %s", interfaceName, udng.GetNetworkName())
+	return deleteUDNManagementPort(interfaceName)
+}
+
+func deleteUDNManagementPort(interfaceName string) error {
 	stdout, stderr, err := util.RunOVSVsctl(
 		"--", "--if-exists", "del-port", "br-int", interfaceName,
 	)
@@ -487,13 +493,64 @@ func (udng *UserDefinedNetworkGateway) deleteUDNManagementPort() error {
 		return fmt.Errorf("failed to delete port %q from br-int, stdout: %q, stderr: %q, error: %w",
 			interfaceName, stdout, stderr, err)
 	}
-	klog.V(3).Infof("Removed OVS management port interface %s for network %s", interfaceName, udng.GetNetworkName())
 	// verify linux device removal - insurance in case something happens with OVS/OVSDB and interface is not removed
 	if link, err := netlink.LinkByName(interfaceName); err == nil {
 		klog.Warningf("Management port interface %s still exists after OVS del-port, deleting manually", interfaceName)
 		err = netlink.LinkDel(link)
 		if err != nil {
 			return fmt.Errorf("failed force remove management port %q, error: %w", interfaceName, err)
+		}
+	}
+	return nil
+}
+
+// CleanupManagementPorts removes all stale UDN ovn-k8s-mpx ports
+func CleanupManagementPorts(ovsClient client.Client, validNetworks ...util.NetInfo) error {
+	// build valid set of mpx interfaces
+	validMpx := sets.New[string]()
+	for _, netInfo := range validNetworks {
+		if netInfo.IsPrimaryNetwork() {
+			validMpx.Insert(util.GetNetworkScopedK8sMgmtHostIntfName(uint(netInfo.GetNetworkID())))
+		}
+	}
+	// insert default network mp0 port
+	validMpx.Insert(util.GetNetworkScopedK8sMgmtHostIntfName(0))
+	// find stale OVS mpx ports
+	p := func(item *vswitchd.Interface) bool {
+		if item.Type == "internal" && !validMpx.Has(item.Name) && strings.HasPrefix(item.Name, types.K8sMgmtIntfNamePrefix) {
+			return true
+		}
+		return false
+	}
+
+	invalidMpx := sets.New[string]()
+	if ovsClient != nil {
+		ovsIfaces, err := ovsops.FindInterfacesWithPredicate(ovsClient, p)
+		if err != nil {
+			return fmt.Errorf("could not search ovs interfaces during node controller manager cleanup: %w", err)
+		}
+		for _, ovsIface := range ovsIfaces {
+			invalidMpx.Insert(ovsIface.Name)
+		}
+	}
+
+	// also for sanity compare with list of stale mpx ports in kernel
+	links, err := netlink.LinkList()
+	if err != nil {
+		return fmt.Errorf("could not list netlink interfaces during node controller manager cleanup: %w", err)
+	}
+
+	for _, link := range links {
+		if !validMpx.Has(link.Attrs().Name) && strings.HasPrefix(link.Attrs().Name, types.K8sMgmtIntfNamePrefix) {
+			invalidMpx.Insert(link.Attrs().Name)
+		}
+	}
+
+	for _, mpx := range invalidMpx.UnsortedList() {
+		// TODO(trozet): eventually convert this to libovsdb - use common code
+		klog.Infof("Removing stale UDN management port: %s", mpx)
+		if err := deleteUDNManagementPort(mpx); err != nil {
+			klog.Errorf("Failed to delete stale management port: %s", mpx)
 		}
 	}
 	return nil
