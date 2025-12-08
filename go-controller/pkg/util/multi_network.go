ca1715b73f47bb32831121bb19f2b98c7b645b83
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2025-12-08 13:35:47 +0100

    Add GetNetworkScopedSwitchToRouterPortName util
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>
    (cherry picked from commit 64279f6e2ecdcedc03a24465c10acb6cb7cd2ee3)

--- go-controller/pkg/util/multi_network.go
+++ go-controller/pkg/util/multi_network.go
@@ -88,6 +88,7 @@ type NetInfo interface {
 	GetNetworkScopedExtPortName(bridgeID, nodeName string) string
 	GetNetworkScopedLoadBalancerName(lbName string) string
 	GetNetworkScopedLoadBalancerGroupName(lbGroupName string) string
+	GetNetworkScopedRouterToSwitchPortName(nodeName string) string
 
 	// GetNetInfo is an identity method used to get the specific NetInfo
 	// implementation
@@ -548,6 +549,10 @@ func (nInfo *DefaultNetInfo) GetNetworkScopedLoadBalancerGroupName(lbGroupName s
 	return nInfo.GetNetworkScopedName(lbGroupName)
 }
 
+func (nInfo *DefaultNetInfo) GetNetworkScopedRouterToSwitchPortName(nodeName string) string {
+	return types.RouterToSwitchPrefix + nInfo.GetNetworkScopedSwitchName(nodeName)
+}
+
 func (nInfo *DefaultNetInfo) canReconcile(netInfo NetInfo) bool {
 	_, ok := netInfo.(*DefaultNetInfo)
 	return ok
@@ -759,6 +764,15 @@ func (nInfo *userDefinedNetInfo) GetNetworkScopedLoadBalancerName(lbName string)
 
 func (nInfo *userDefinedNetInfo) GetNetworkScopedLoadBalancerGroupName(lbGroupName string) string {
 	return nInfo.GetNetworkScopedName(lbGroupName)
+}
+
+// GetNetworkScopedRouterToSwitchPortName returns the port name from router to switch.
+// For Layer2 topology, this is the transit router to switch port (trtos-).
+// For Layer3 topology, this is the router to switch port (rtos-).
+// Not Applicable for Localnet topology.
+func (nInfo *userDefinedNetInfo) GetNetworkScopedRouterToSwitchPortName(nodeName string) string {
+	switchName := nInfo.GetNetworkScopedSwitchName(nodeName)
+	return types.RouterToSwitchPrefix + switchName
 }
 
 // getPrefix returns if the logical entities prefix for this network
