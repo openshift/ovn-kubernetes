04f3fc5403f1136fa935083962a333370dca7827
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2026-03-02 11:56:35 -0500

    Bump frr image from frr-k8s demo to 10.4.2
    This is expected to help with some EVPN related failures.
    
    Signed-off-by: Ihar Hrachyshka <ihrachyshka@nvidia.com>

--- contrib/kind-common.sh
+++ contrib/kind-common.sh
@@ -957,8 +957,11 @@ clone_frr() {
     # (https://github.com/FRRouting/frr/issues/15699, fixed in FRR 10.1 by
     # https://github.com/FRRouting/frr/pull/15714).
     #
-    # TODO: remove this sed once https://github.com/metallb/frr-k8s/pull/404 is merged.
-    sed -i 's|quay.io/frrouting/frr:9.1.0|quay.io/frrouting/frr:10.4.1|g' hack/demo/demo.sh
+    # Bump to 10.4.1 for upstream demo was posted here: https://github.com/metallb/frr-k8s/pull/404
+    # We bump further to 10.4.2 to include additional fixes for EVPN:
+    # https://github.com/ovn-kubernetes/ovn-kubernetes/pull/5874#issuecomment-3907335193
+    # https://github.com/ovn-kubernetes/ovn-kubernetes/pull/5874#issuecomment-3898408592
+    sed -i 's|quay.io/frrouting/frr:9.1.0|quay.io/frrouting/frr:10.4.2|g' hack/demo/demo.sh
 
     popd
 
