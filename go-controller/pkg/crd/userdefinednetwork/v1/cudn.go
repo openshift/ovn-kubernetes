272e9873bc2af17cab5c9c8a2a00cb277f9a4f18
Dave Tucker <dave@dtucker.co.uk>
2026-03-05 16:40:26 +0000

    fix: add short names for (c)udn
    typing clusteruserdefinednetwork is getting old fast :)
    seems that cudn and udn would be much nicer for all users.
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- go-controller/pkg/crd/userdefinednetwork/v1/cudn.go
+++ go-controller/pkg/crd/userdefinednetwork/v1/cudn.go
@@ -7,7 +7,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +genclient
 // +genclient:nonNamespaced
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +kubebuilder:resource:path=clusteruserdefinednetworks,scope=Cluster
+// +kubebuilder:resource:path=clusteruserdefinednetworks,scope=Cluster,shortName=cudn
 // +kubebuilder:singular=clusteruserdefinednetwork
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
