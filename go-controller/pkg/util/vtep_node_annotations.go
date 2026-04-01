ac618f13027f8420e2852f8578386c01e1ba0309
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2026-04-01 18:52:18 +0200

    Add new VTEP annotation utils
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

---
+++ go-controller/pkg/util/vtep_node_annotations.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	OVNNodeVTEPs = "k8s.ovn.org/vteps"
+)
+
+// VTEPNodeAnnotation holds the VTEP IPs discovered or allocated on a node.
+// The annotation on the node is a JSON map keyed by VTEP name:
+//
+//	{"vtep-name1": {"ips": ["192.168.1.1", "fd00::1"]}, "vtep-name2": {"ips": ["10.0.0.1"]}}
+type VTEPNodeAnnotation struct {
+	IPs []string `json:"ips"`
+}
+
+// ParseNodeVTEPs parses the k8s.ovn.org/vteps annotation from a node and
+// returns a map of VTEP name to its annotation data. Returns an
+// AnnotationNotSetError if the annotation is not present.
+func ParseNodeVTEPs(node *corev1.Node) (map[string]VTEPNodeAnnotation, error) {
+	raw, ok := node.Annotations[OVNNodeVTEPs]
+	if !ok {
+		return nil, newAnnotationNotSetError("%s annotation not found for node %q", OVNNodeVTEPs, node.Name)
+	}
+	var vteps map[string]VTEPNodeAnnotation
+	if err := json.Unmarshal([]byte(raw), &vteps); err != nil {
+		return nil, fmt.Errorf("failed to parse %s annotation on node %q: %w", OVNNodeVTEPs, node.Name, err)
+	}
+	return vteps, nil
+}
+
+// NodeVTEPsAnnotationChanged returns true if the k8s.ovn.org/vteps annotation
+// differs between the old and new node objects. Both oldNode and newNode must
+// be non-nil;
+func NodeVTEPsAnnotationChanged(oldNode, newNode *corev1.Node) bool {
+	return oldNode.Annotations[OVNNodeVTEPs] != newNode.Annotations[OVNNodeVTEPs]
+}
