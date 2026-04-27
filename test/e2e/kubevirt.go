6bc965b5c3d0c82aa9acae1aa7b542d06e168284
Enrique Llorente <ellorent@redhat.com>
2026-04-27 10:21:16 +0200

    e2e(virt): Replace fake virt-launcher pod with proper VMI in UDPN test
    Replace the fake virt-launcher pod with a proper VMI using
    fedoraWithTestToolingVMI and l2bridge binding plugin. This is needed
    because now ovnk requires the same labels as IPAM controller, so now
    IPAM controller is activated by this tests and fails since there is no
    vm.
    
    This also allow us to remove dependency on nmstate build image
    quay.io/nmstate/c10s-nmstate-dev:latest
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- test/e2e/kubevirt.go
+++ test/e2e/kubevirt.go
@@ -53,7 +53,6 @@ import (
 	e2enode "k8s.io/kubernetes/test/e2e/framework/node"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	e2eskipper "k8s.io/kubernetes/test/e2e/framework/skipper"
-	testutils "k8s.io/kubernetes/test/utils"
 	utilnet "k8s.io/utils/net"
 	"k8s.io/utils/ptr"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -2355,40 +2354,30 @@ ip route add %[3]s via %[4]s
 	})
 	Context("with kubevirt VM using layer2 UDPN", Ordered, func() {
 		var (
-			podName                 = "virt-launcher-vm1"
-			cidrIPv4                = "172.31.0.0/24"
-			cidrIPv6                = "2010:100:200::/60"
-			primaryUDNNetworkStatus nadapi.NetworkStatus
-			virtLauncherCommand     = func(command string) (string, error) {
-				stdout, stderr, err := ExecShellInPodWithFullOutput(fr, namespace, podName, command)
-				if err != nil {
-					return "", fmt.Errorf("%s: %s: %w", stdout, stderr, err)
-				}
-				return stdout, nil
+			cidrIPv4   = "172.31.0.0/24"
+			cidrIPv6   = "2010:100:200::/60"
+			vmi        *kubevirtv1.VirtualMachineInstance
+			vmiCommand = func(command string) (string, error) {
+				return virtClient.RunCommand(vmi, command, 5*time.Second)
 			}
-			primaryUDNValueFor = func(ty, field string) ([]string, error) {
-				output, err := virtLauncherCommand(fmt.Sprintf(`nmcli -e no -g %s %s show ovn-udn1`, field, ty))
+			primaryUDNDeviceValue = func(field string) ([]string, error) {
+				output, err := vmiCommand(fmt.Sprintf(`nmcli -e no -g %s device show eth0`, field))
 				if err != nil {
 					return nil, err
 				}
+				output = strings.ReplaceAll(output, "\r", "")
 				return strings.Split(output, " | "), nil
-			}
-			primaryUDNValueForConnection = func(field string) ([]string, error) {
-				return primaryUDNValueFor("connection", field)
-			}
-			primaryUDNValueForDevice = func(field string) ([]string, error) {
-				return primaryUDNValueFor("device", field)
 			}
 		)
 		AfterAll(func() {
-			Expect(removeImagesInNodes(kubevirt.FakeLauncherImage)).To(Succeed())
+			Expect(removeImagesInNodes(kubevirt.FedoraWithTestToolingContainerDiskImage)).To(Succeed())
 		})
 		BeforeEach(func() {
 			ns, err := fr.CreateNamespace(context.TODO(), fr.BaseName, map[string]string{
 				"e2e-framework":           fr.BaseName,
 				RequiredUDNNamespaceLabel: "",
 			})
-			Expect(err).ToNot(HaveOccurred())
+			Expect(err).NotTo(HaveOccurred())
 			fr.Namespace = ns
 			namespace = fr.Namespace.Name
 			dualCIDRs := filterDualStackCIDRs(fr.ClientSet, []udnv1.CIDR{udnv1.CIDR(cidrIPv4), udnv1.CIDR(cidrIPv6)})
@@ -2396,44 +2385,38 @@ ip route add %[3]s via %[4]s
 			cudn.Spec.Network.Layer2.MTU = 1300
 			createCUDN(cudn)
 
-			By("Create virt-launcher pod")
-			kubevirtPod := kubevirt.GenerateFakeVirtLauncherPod(namespace, "vm1")
-			Expect(crClient.Create(context.Background(), kubevirtPod)).To(Succeed())
+			By("Create VMI with primary UDN")
+			networkData := `version: 2
+ethernets:
+  eth0:
+    dhcp4: true
+    dhcp6: true
+    ipv6-address-generation: eui64`
+			vmi = fedoraWithTestToolingVMI(nil /*labels*/, nil /*annotations*/, nil, /*nodeSelector*/
+				kubevirtv1.NetworkSource{
+					Pod: &kubevirtv1.PodNetwork{},
+				}, "#", networkData)
+			vmi.Spec.Domain.Devices.Interfaces[0].Bridge = nil
+			vmi.Spec.Domain.Devices.Interfaces[0].Binding = &kubevirtv1.PluginBinding{Name: "l2bridge"}
+			createVirtualMachineInstance(vmi)
 
-			By("Wait for virt-launcher pod to be ready and primary UDN network status to pop up")
-			waitForPodsCondition([]*corev1.Pod{kubevirtPod}, func(g Gomega, pod *corev1.Pod) {
-				ok, err := testutils.PodRunningReady(pod)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(ok).To(BeTrue())
+			By("Wait for VMI to be ready")
+			waitVirtualMachineInstanceReadiness(vmi)
+			Expect(crClient.Get(context.Background(), crclient.ObjectKeyFromObject(vmi), vmi)).To(Succeed())
 
-				primaryUDNNetworkStatuses, err := podNetworkStatus(pod, func(networkStatus nadapi.NetworkStatus) bool {
-					return networkStatus.Default
-				})
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(primaryUDNNetworkStatuses).To(HaveLen(1))
-				primaryUDNNetworkStatus = primaryUDNNetworkStatuses[0]
-			})
-
-			By("Wait NetworkManager readiness")
-			Eventually(func() error {
-				_, err := virtLauncherCommand("systemctl is-active NetworkManager")
-				return err
-			}).
-				WithTimeout(5 * time.Second).
-				WithPolling(time.Second).
-				Should(Succeed())
-
-			By("Reconfigure primary UDN interface to use dhcp/nd for ipv4 and ipv6")
-			_, err = virtLauncherCommand(kubevirt.GenerateAddressDiscoveryConfigurationCommand("ovn-udn1"))
-			Expect(err).NotTo(HaveOccurred())
+			By("Login to the VM console")
+			Expect(virtClient.LoginToFedora(vmi, "fedora", "fedora")).To(Succeed())
 		})
 		It("should configure IPv4 and IPv6 using DHCP and NDP", func() {
 			dnsService, err := fr.ClientSet.CoreV1().Services(config.Kubernetes.DNSServiceNamespace).
 				Get(context.Background(), config.Kubernetes.DNSServiceName, metav1.GetOptions{})
 			Expect(err).NotTo(HaveOccurred())
 
+			expectedNumberOfAddresses := len(filterDualStackCIDRs(fr.ClientSet, []udnv1.CIDR{udnv1.CIDR(cidrIPv4), udnv1.CIDR(cidrIPv6)}))
+			vmiAddresses := virtualMachineAddressesFromStatus(vmi, expectedNumberOfAddresses)
+
 			if isIPv4Supported(fr.ClientSet) {
-				expectedIP, err := matchIPv4StringFamily(primaryUDNNetworkStatus.IPs)
+				expectedIP, err := matchIPv4StringFamily(vmiAddresses)
 				Expect(err).NotTo(HaveOccurred())
 
 				expectedDNS, err := matchIPv4StringFamily(dnsService.Spec.ClusterIPs)
@@ -2443,40 +2426,40 @@ ip route add %[3]s via %[4]s
 				Expect(err).NotTo(HaveOccurred())
 				expectedGateway := util.GetNodeGatewayIfAddr(cidr).IP.String()
 
-				Eventually(primaryUDNValueForConnection).
+				Eventually(primaryUDNDeviceValue).
 					WithArguments("DHCP4.OPTION").
 					WithTimeout(10 * time.Second).
 					WithPolling(time.Second).
 					Should(ContainElements(
-						"host_name = vm1",
+						fmt.Sprintf("host_name = %s", vmi.Name),
 						fmt.Sprintf("ip_address = %s", expectedIP),
 						fmt.Sprintf("domain_name_servers = %s", expectedDNS),
 						fmt.Sprintf("routers = %s", expectedGateway),
 						"interface_mtu = 1300",
 					))
-				Expect(primaryUDNValueForConnection("IP4.ADDRESS")).To(ConsistOf(expectedIP + "/24"))
-				Expect(primaryUDNValueForConnection("IP4.GATEWAY")).To(ConsistOf(expectedGateway))
-				Expect(primaryUDNValueForConnection("IP4.DNS")).To(ConsistOf(expectedDNS))
-				Expect(primaryUDNValueForDevice("GENERAL.MTU")).To(ConsistOf("1300"))
+				Expect(primaryUDNDeviceValue("IP4.ADDRESS")).To(ConsistOf(expectedIP + "/24"))
+				Expect(primaryUDNDeviceValue("IP4.GATEWAY")).To(ConsistOf(expectedGateway))
+				Expect(primaryUDNDeviceValue("IP4.DNS")).To(ConsistOf(expectedDNS))
+				Expect(primaryUDNDeviceValue("GENERAL.MTU")).To(ConsistOf("1300"))
 			}
 
 			if isIPv6Supported(fr.ClientSet) {
-				expectedIP, err := matchIPv6StringFamily(primaryUDNNetworkStatus.IPs)
+				expectedIP, err := matchIPv6StringFamily(vmiAddresses)
 				Expect(err).NotTo(HaveOccurred())
-				Eventually(primaryUDNValueFor).
-					WithArguments("connection", "DHCP6.OPTION").
+				Eventually(primaryUDNDeviceValue).
+					WithArguments("DHCP6.OPTION").
 					WithTimeout(10 * time.Second).
 					WithPolling(time.Second).
 					Should(ContainElements(
-						"fqdn_fqdn = vm1",
+						fmt.Sprintf("fqdn_fqdn = %s", vmi.Name),
 						fmt.Sprintf("ip6_address = %s", expectedIP),
 					))
-				Expect(primaryUDNValueForConnection("IP6.ADDRESS")).To(SatisfyAll(HaveLen(2), ContainElements(expectedIP+"/128")))
-				Expect(primaryUDNValueForConnection("IP6.GATEWAY")).To(ConsistOf(WithTransform(func(ipv6 string) bool {
+				Expect(primaryUDNDeviceValue("IP6.ADDRESS")).To(SatisfyAll(HaveLen(2), ContainElements(expectedIP+"/128")))
+				Expect(primaryUDNDeviceValue("IP6.GATEWAY")).To(ConsistOf(WithTransform(func(ipv6 string) bool {
 					return netip.MustParseAddr(ipv6).IsLinkLocalUnicast()
 				}, BeTrue())))
-				Expect(primaryUDNValueForConnection("IP6.ROUTE")).To(ContainElement(ContainSubstring(fmt.Sprintf("dst = %s", cidrIPv6))))
-				Expect(primaryUDNValueForDevice("GENERAL.MTU")).To(ConsistOf("1300"))
+				Expect(primaryUDNDeviceValue("IP6.ROUTE")).To(ContainElement(ContainSubstring(fmt.Sprintf("dst = %s", cidrIPv6))))
+				Expect(primaryUDNDeviceValue("GENERAL.MTU")).To(ConsistOf("1300"))
 			}
 		})
 	})
