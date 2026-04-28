ed231b91ad6ff81f5aac9476eb5f41242cefa7bf
Mykola Yurchenko <myurchenko@nvidia.com>
2026-04-27 20:14:22 -0700

    node/bridgeconfig: migrate bridgedGatewayNodeSetup to libovsdb
    Replace ovs-vsctl get/set of external_ids:ovn-bridge-mappings bash
    calls with libovsdbops.UpdateOpenvSwitchExternalIDs.
    
    Signed-off-by: Mykola Yurchenko <myurchenko@nvidia.com>

--- go-controller/pkg/node/gateway_shared_intf.go
+++ go-controller/pkg/node/gateway_shared_intf.go
@@ -1697,7 +1697,7 @@ func newGateway(
 
 	advertised := util.IsPodNetworkAdvertisedAtNode(networkManager.GetNetwork(types.DefaultNetworkName), nodeName)
 	gwBridge, exGwBridge, err := gatewayInitInternal(
-		nodeName, gwIntf, egressGWIntf, gwNextHops, subnets, gwIPs, advertised, nodeAnnotator)
+		ovsClient, nodeName, gwIntf, egressGWIntf, gwNextHops, subnets, gwIPs, advertised, nodeAnnotator)
 	if err != nil {
 		return nil, err
 	}
