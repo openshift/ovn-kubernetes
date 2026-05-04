adaece03c7ff7ff4491456365524e039a67f94c4
Lei Huang <huang.f.lei@gmail.com>
2026-05-04 00:47:53 -0700

    UDN: reconcile BGP isolation on primary UDN subnet changes
    Queue advertised local nodes for reconciliation when a network subnet list
    changes. This ensures BGP Network Isolation ACLs are rebuilt when a primary
    Layer3 UDN gains an additional subnet.
    
    Add unit coverage for both the reconcile trigger and the advertised network
    isolation pass ACL update with the added subnet.
    
    Signed-off-by: Lei Huang <huang.f.lei@gmail.com>

--- go-controller/pkg/ovn/udn_isolation_test.go
+++ go-controller/pkg/ovn/udn_isolation_test.go
@@ -4,12 +4,19 @@
 package ovn
 
 import (
+	"strings"
+
+	nettypes "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/config"
 	libovsdbops "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/libovsdb/ops"
 	libovsdbutil "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/libovsdb/util"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/nbdb"
 	libovsdbtest "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/testing/libovsdb"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/util"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -91,5 +98,119 @@ var _ = Describe("UDN Isolation", func() {
 		Expect(acls[0].ExternalIDs).To(Equal(fakeController.getUDNACLDbIDs(denyPrimaryUDNACL, libovsdbutil.ACLEgress).GetExternalIDs()))
 		By("expect updated ACL with proper name")
 		Expect(*acls[0].Name).To(BeEmpty())
+	})
+
+	It("queues advertised local nodes when a primary Layer3 UDN adds a subnet", func() {
+		config.OVNKubernetesFeature.EnableMultiNetwork = true
+		config.OVNKubernetesFeature.EnableNetworkSegmentation = true
+		config.OVNKubernetesFeature.EnableDynamicUDNAllocation = true
+		config.OVNKubernetesFeature.AdvertisedUDNIsolationMode = config.AdvertisedUDNIsolationModeStrict
+		config.IPv4Mode = true
+		config.IPv6Mode = false
+
+		fakeOVN := NewFakeOVN(true)
+
+		netInfoBefore := dummyPrimaryLayer3UserDefinedNetwork("192.168.0.0/16", "192.168.1.0/24")
+		nadBefore, err := newNetworkAttachmentDefinition(ns, nadName, *netInfoBefore.netconf())
+		Expect(err).NotTo(HaveOccurred())
+		node, err := newNodeWithUserDefinedNetworks(nodeName, "192.168.126.202/24", netInfoBefore)
+		Expect(err).NotTo(HaveOccurred())
+
+		fakeOVN.startWithDBSetup(
+			libovsdbtest.TestSetup{},
+			node,
+			&nettypes.NetworkAttachmentDefinitionList{Items: []nettypes.NetworkAttachmentDefinition{*nadBefore}},
+		)
+		defer fakeOVN.shutdown()
+
+		l3Controller, ok := fakeOVN.fullL3UDNControllers[netInfoBefore.netName]
+		Expect(ok).To(BeTrue())
+		mutableNetInfo := util.NewMutableNetInfo(l3Controller.GetNetInfo())
+		mutableNetInfo.SetNetworkID(2)
+		mutableNetInfo.SetPodNetworkAdvertisedVRFs(map[string][]string{nodeName: {"vrf"}})
+		Expect(util.ReconcileNetInfo(l3Controller.ReconcilableNetInfo, mutableNetInfo)).To(Succeed())
+		l3Controller.lastNADKeys = sets.New[string](ns + "/" + nadName)
+
+		netInfoAfter := dummyPrimaryLayer3UserDefinedNetwork("192.168.0.0/16,192.169.0.0/16", "192.168.1.0/24")
+		nadAfter, err := newNetworkAttachmentDefinition(ns, nadName, *netInfoAfter.netconf())
+		Expect(err).NotTo(HaveOccurred())
+		parsedNetInfoAfter, err := util.ParseNADInfo(nadAfter)
+		Expect(err).NotTo(HaveOccurred())
+		mutableNetInfoAfter := util.NewMutableNetInfo(parsedNetInfoAfter)
+		mutableNetInfoAfter.SetNetworkID(2)
+		mutableNetInfoAfter.SetPodNetworkAdvertisedVRFs(map[string][]string{nodeName: {"vrf"}})
+
+		reconciledNodes := sets.New[string]()
+		Expect(l3Controller.reconcile(mutableNetInfoAfter, func(node string) {
+			reconciledNodes.Insert(node)
+		})).To(Succeed())
+		Expect(reconciledNodes.Has(nodeName)).To(BeTrue())
+	})
+
+	It("updates advertised network isolation ACLs when a primary Layer3 UDN adds a subnet", func() {
+		config.OVNKubernetesFeature.EnableMultiNetwork = true
+		config.OVNKubernetesFeature.EnableNetworkSegmentation = true
+		config.OVNKubernetesFeature.EnableDynamicUDNAllocation = true
+		config.OVNKubernetesFeature.AdvertisedUDNIsolationMode = config.AdvertisedUDNIsolationModeStrict
+		config.IPv4Mode = true
+		config.IPv6Mode = false
+
+		fakeOVN := NewFakeOVN(true)
+
+		netInfoBefore := dummyPrimaryLayer3UserDefinedNetwork("192.168.0.0/16", "192.168.1.0/24")
+		nadBefore, err := newNetworkAttachmentDefinition(ns, nadName, *netInfoBefore.netconf())
+		Expect(err).NotTo(HaveOccurred())
+		node, err := newNodeWithUserDefinedNetworks(nodeName, "192.168.126.202/24", netInfoBefore)
+		Expect(err).NotTo(HaveOccurred())
+
+		fakeOVN.startWithDBSetup(
+			libovsdbtest.TestSetup{},
+			node,
+			&nettypes.NetworkAttachmentDefinitionList{Items: []nettypes.NetworkAttachmentDefinition{*nadBefore}},
+		)
+		defer fakeOVN.shutdown()
+
+		l3Controller, ok := fakeOVN.fullL3UDNControllers[netInfoBefore.netName]
+		Expect(ok).To(BeTrue())
+		mutableNetInfo := util.NewMutableNetInfo(l3Controller.GetNetInfo())
+		mutableNetInfo.SetNetworkID(2)
+		mutableNetInfo.SetPodNetworkAdvertisedVRFs(map[string][]string{nodeName: {"vrf"}})
+		Expect(util.ReconcileNetInfo(l3Controller.ReconcilableNetInfo, mutableNetInfo)).To(Succeed())
+
+		_, err = l3Controller.addressSetFactory.EnsureAddressSet(GetAdvertisedNetworkSubnetsAddressSetDBIDs())
+		Expect(err).NotTo(HaveOccurred())
+		Expect(libovsdbops.CreateOrUpdateLogicalSwitch(fakeOVN.nbClient, &nbdb.LogicalSwitch{
+			Name: l3Controller.GetNetworkScopedSwitchName(nodeName),
+		})).To(Succeed())
+
+		Expect(l3Controller.addAdvertisedNetworkIsolation(nodeName)).To(Succeed())
+		getPassACL := func() *nbdb.ACL {
+			acls, err := libovsdbops.FindACLsWithPredicate(fakeOVN.nbClient, func(acl *nbdb.ACL) bool {
+				return acl.Action == nbdb.ACLActionPass &&
+					acl.ExternalIDs[libovsdbops.ObjectNameKey.String()] == netInfoBefore.netName
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(acls).To(HaveLen(1))
+			return acls[0]
+		}
+		Expect(getPassACL().Match).To(ContainSubstring("192.168.0.0/16"))
+		Expect(getPassACL().Match).NotTo(ContainSubstring("192.169.0.0/16"))
+
+		netInfoAfter := dummyPrimaryLayer3UserDefinedNetwork("192.168.0.0/16,192.169.0.0/16", "192.168.1.0/24")
+		nadAfter, err := newNetworkAttachmentDefinition(ns, nadName, *netInfoAfter.netconf())
+		Expect(err).NotTo(HaveOccurred())
+		parsedNetInfoAfter, err := util.ParseNADInfo(nadAfter)
+		Expect(err).NotTo(HaveOccurred())
+		mutableNetInfoAfter := util.NewMutableNetInfo(parsedNetInfoAfter)
+		mutableNetInfoAfter.SetNetworkID(2)
+		mutableNetInfoAfter.SetPodNetworkAdvertisedVRFs(map[string][]string{nodeName: {"vrf"}})
+		Expect(util.ReconcileNetInfo(l3Controller.ReconcilableNetInfo, mutableNetInfoAfter)).To(Succeed())
+
+		Expect(l3Controller.addAdvertisedNetworkIsolation(nodeName)).To(Succeed())
+		passACLMatch := getPassACL().Match
+		Expect(passACLMatch).To(ContainSubstring("192.168.0.0/16"))
+		Expect(passACLMatch).To(ContainSubstring("192.169.0.0/16"))
+		Expect(strings.Count(passACLMatch, "ip4.src ==")).To(Equal(2))
+		Expect(strings.Count(passACLMatch, "ip4.dst ==")).To(Equal(2))
 	})
 })
