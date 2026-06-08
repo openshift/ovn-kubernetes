openshift/ovn-kubernetes

commit 1826e997116edb6682de9b4a1c16523582c60a4a

Move primary UDN out of pod request
Now pod requests are created at the Server level, not one
podRequest inside another podRequest handling.
Most of the primary UDN-related functions were just copies
and strange combinations of the regular podRequest handling.

Update tests to use cni Server handler instead of mocks.

Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>

go-controller/pkg/cni/cniserver.go
@@ -15,15 +15,20 @@ import (
 	"time"
 
 	cnitypes "github.com/containernetworking/cni/pkg/types"
+	current "github.com/containernetworking/cni/pkg/types/100"
 	"github.com/gorilla/mux"
 	nadv1Listers "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/listers/k8s.cni.cncf.io/v1"
+	nadutils "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/utils"
 
 	"k8s.io/client-go/kubernetes"
 	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 
 	"github.com/ovn-kubernetes/libovsdb/client"
 
+	ovncnitypes "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/cni/types"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/cni/udn"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/metrics"
@@ -33,6 +38,7 @@ import (
 )
 
 const kubeletDefaultCRIOperationTimeout = 2 * time.Minute
+const resourceNameAnnot = "k8s.v1.cni.cncf.io/resourceName"
 
 // *** The Server is PRIVATE API between OVN components and may be
 // changed at any time.  It is in no way a supported interface or API. ***
@@ -213,26 +219,35 @@ func cniRequestToPodRequest(cr *Request, ctx context.Context) (*PodRequest, erro
 	req.netName = conf.Name
 	if req.netName == types.DefaultNetworkName {
 		req.nadName = types.DefaultNetworkName
+		req.nadKey = types.DefaultNetworkName
 	} else {
 		req.nadName = conf.NADName
 	}
 
-	if conf.DeviceID != "" {
-		if util.IsPCIDeviceName(conf.DeviceID) {
-			// DeviceID is a PCI address
-			req.IsVFIO = util.GetSriovnetOps().IsVfPciVfioBound(conf.DeviceID)
-		} else if util.IsAuxDeviceName(conf.DeviceID) {
-			// DeviceID is an Auxiliary device name - <driver_name>.<kind_of_a_type>.<id>
-			chunks := strings.Split(conf.DeviceID, ".")
-			if len(chunks) < 2 {
-				return nil, fmt.Errorf("invalid auxiliary device name %q: expected driver.<type>.<id>", conf.DeviceID)
-			}
-			if chunks[1] != "sf" {
-				return nil, fmt.Errorf("only SF auxiliary devices are supported, device name %q is not supported", conf.DeviceID)
-			}
-		} // else it is a netdev name, which is used for simulated DPU environments.
+	if err = updateDeviceInfo(req); err != nil {
+		return nil, err
 	}
 	return req, nil
+}
+
+func updateDeviceInfo(pr *PodRequest) error {
+	if pr.CNIConf.DeviceID == "" {
+		return nil
+	}
+	if util.IsPCIDeviceName(pr.CNIConf.DeviceID) {
+		// DeviceID is a PCI address
+		pr.IsVFIO = util.GetSriovnetOps().IsVfPciVfioBound(pr.CNIConf.DeviceID)
+	} else if util.IsAuxDeviceName(pr.CNIConf.DeviceID) {
+		// DeviceID is an Auxiliary device name - <driver_name>.<kind_of_a_type>.<id>
+		chunks := strings.Split(pr.CNIConf.DeviceID, ".")
+		if len(chunks) < 2 {
+			return fmt.Errorf("invalid auxiliary device name %q: expected driver.<type>.<id>", pr.CNIConf.DeviceID)
+		}
+		if chunks[1] != "sf" {
+			return fmt.Errorf("only SF auxiliary devices are supported, device name %q is not supported", pr.CNIConf.DeviceID)
+		}
+	} // else it is a netdev name, which is used for simulated DPU environments.
+	return nil
 }
 
 // Dispatch a pod request to the request handler and return the result to the
@@ -296,7 +311,7 @@ func (s *Server) handleCNIRequest(r *http.Request) (result []byte, err error) {
 	klog.Infof("%s %s starting CNI request", request, request.Command)
 	switch request.Command {
 	case CNIAdd:
-		response, err = request.cmdAdd(s.kubeAuth, s.clientSet, s.networkManager, s.ovsClient)
+		response, err = request.cmdAdd(s.kubeAuth, s.clientSet, s.ovsClient)
 	case CNIDel:
 		response, err = request.cmdDel(s.clientSet)
 	default:
@@ -307,11 +322,141 @@ func (s *Server) handleCNIRequest(r *http.Request) (result []byte, err error) {
 		return nil, err
 	}
 
+	// check if this is a default network request for a pod with primary UDN
+	// and create a primary interface if so
+	if request.Command == CNIAdd {
+		// We don't do anything extra for CNIDel, because UnconfigureInterface already deletes all ports
+		// for a given pod from OVS, also some other cleanup are done in batch in cmdDel
+		primaryPodRequest, err := s.getPrimaryUDNPodRequest(request)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get primary UDN pod request: %v", err)
+		}
+		if primaryPodRequest != nil {
+			klog.V(4).Infof("Pod %s/%s primaryUDN podRequest %v", primaryPodRequest.PodNamespace, primaryPodRequest.PodName, primaryPodRequest)
+			primaryResponse, err := primaryPodRequest.cmdAdd(s.kubeAuth, s.clientSet, s.ovsClient)
+			if err != nil {
+				return nil, fmt.Errorf("failed to add primary UDN pod request: %v", err)
+			}
+			// merge primary response into the original response
+			mergePrimaryUDNResponse(response, primaryResponse, primaryPodRequest)
+		}
+	}
+
 	if result, err = response.Marshal(); err != nil {
 		return nil, fmt.Errorf("failed to marshal result: %v", err)
 	}
 
 	return result, nil
+}
+
+func (s *Server) getPrimaryUDNPodRequest(originalPodRequest *PodRequest) (*PodRequest, error) {
+	if !util.IsNetworkSegmentationSupportEnabled() {
+		return nil, nil
+	}
+	// check if this a default network request for a pod with primary UDN
+	// and create a primary interface if so
+	if originalPodRequest.nadName != types.DefaultNetworkName {
+		return nil, nil
+	}
+	podNamespace := originalPodRequest.PodNamespace
+	podName := originalPodRequest.PodName
+	// this function is only called after the default network is set up, so the pod should already be in the
+	// network manager's cache and have an active network
+	activeNetwork, err := s.networkManager.GetActiveNetworkForNamespace(podNamespace)
+	if err != nil {
+		return nil, err
+	}
+	// CNI should always have an active network for a pod on our node
+	if activeNetwork == nil {
+		return nil, fmt.Errorf("no active network found for namespace %s", podNamespace)
+	}
+	if activeNetwork.IsDefault() {
+		// there is no primary NAD
+		return nil, nil
+	}
+	primaryNADKey, err := s.networkManager.GetPrimaryNADForNamespace(podNamespace)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get primary NAD for namespace %s: %v", podNamespace, err)
+	}
+
+	primaryPodRequest := &PodRequest{
+		Command:      originalPodRequest.Command,
+		PodNamespace: originalPodRequest.PodNamespace,
+		PodName:      originalPodRequest.PodName,
+		PodUID:       originalPodRequest.PodUID,
+		SandboxID:    originalPodRequest.SandboxID,
+		Netns:        originalPodRequest.Netns,
+		IfName:       "ovn-udn1",
+		timestamp:    time.Now(),
+		ctx:          originalPodRequest.ctx,
+		CNIConf: &ovncnitypes.NetConf{
+			// primary UDN MTU will be taken from config.Default.MTU
+			// if not specified at the NAD
+			MTU: activeNetwork.MTU(),
+		},
+		netName: activeNetwork.GetNetworkName(),
+		nadName: primaryNADKey,
+		nadKey:  primaryNADKey,
+	}
+	// To support for non VFIO devices like SRIOV, get the primary UDN's resource name
+	nadNamespace, nadName, err := cache.SplitMetaNamespaceKey(primaryNADKey)
+	if err != nil {
+		return nil, fmt.Errorf("invalid NAD name %s", primaryNADKey)
+	}
+	nad, err := s.clientSet.nadLister.NetworkAttachmentDefinitions(nadNamespace).Get(nadName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get primary UDN's network-attachment-definition %s: %v", nadName, err)
+	}
+	resourceName := nad.Annotations[resourceNameAnnot]
+	if resourceName != "" {
+		pod, err := s.clientSet.getPod(podNamespace, podName)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get pod %s/%s: %v", podNamespace, podName, err)
+		}
+		deviceID, err := udn.GetPodPrimaryUDNDeviceID(pod, resourceName)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get primary UDN device ID for pod %s/%s resource %s: %v",
+				pod.Namespace, pod.Name, resourceName, err)
+		}
+		primaryPodRequest.CNIConf.DeviceID = deviceID
+		deviceInfo, err := nadutils.LoadDeviceInfoFromDP(resourceName, deviceID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load primary UDN's device info for pod %s/%s resource %s deviceID %s: %w",
+				pod.Namespace, pod.Name, resourceName, deviceID, err)
+		}
+		primaryPodRequest.deviceInfo = *deviceInfo
+	}
+	if err = updateDeviceInfo(primaryPodRequest); err != nil {
+		return nil, err
+	}
+	return primaryPodRequest, nil
+}
+
+func mergePrimaryUDNResponse(originalResponse, primaryResponse *Response, primaryPodRequest *PodRequest) {
+	// merge primary response into the original response
+	if originalResponse == nil || primaryResponse == nil {
+		return
+	}
+	if !config.UnprivilegedMode {
+		result := originalResponse.Result
+		primaryUDNResult := primaryResponse.Result
+		result.Routes = append(result.Routes, primaryUDNResult.Routes...)
+		numOfInitialIPs := len(result.IPs)
+		numOfInitialIfaces := len(result.Interfaces)
+		result.Interfaces = append(result.Interfaces, primaryUDNResult.Interfaces...)
+		result.IPs = append(result.IPs, primaryUDNResult.IPs...)
+
+		// Offset the index of the default network IPs to correctly point to the default network interfaces
+		for i := numOfInitialIPs; i < len(result.IPs); i++ {
+			ifaceIPConfig := result.IPs[i].Copy()
+			if result.IPs[i].Interface != nil {
+				result.IPs[i].Interface = current.Int(*ifaceIPConfig.Interface + numOfInitialIfaces)
+			}
+		}
+	} else {
+		originalResponse.PrimaryUDNPodReq = primaryPodRequest
+		originalResponse.PrimaryUDNPodInfo = primaryResponse.PodIFInfo
+	}
 }
 
 func (s *Server) handleCNIMetrics(w http.ResponseWriter, r *http.Request) {

go-controller/pkg/cni/cni.go
@@ -7,26 +7,21 @@ import (
 	"fmt"
 	"net"
 	"strings"
-	"time"
 
 	current "github.com/containernetworking/cni/pkg/types/100"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
-	corev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/klog/v2"
 	utilnet "k8s.io/utils/net"
 
 	"github.com/ovn-kubernetes/libovsdb/client"
 
-	ovncnitypes "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/cni/types"
-	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/cni/udn"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/kube"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/kubevirt"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/libovsdb/ops/ovs"
-	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/util"
 )
@@ -116,43 +111,7 @@ func (pr *PodRequest) checkOrUpdatePodUID(pod *corev1.Pod) error {
 	return nil
 }
 
-func (pr *PodRequest) cmdAdd(kubeAuth *KubeAPIAuth, clientset *ClientSet, networkManager networkmanager.Interface, ovsClient client.Client) (*Response, error) {
-	return pr.cmdAddWithGetCNIResultFunc(kubeAuth, clientset, getCNIResult, networkManager, ovsClient)
-}
-
-// primaryDPUReady makes sure previous annotation condition is ready, then if primary UDN interface is needed and it is
-// in the DPU-HOST/DPU setup, checks if DPU connection annotations for primary UDN interface are ready.
-func (pr *PodRequest) primaryDPUReady(primaryUDN *udn.UserDefinedPrimaryNetwork, k kube.Interface, podLister corev1listers.PodLister, annotCondFn podAnnotWaitCond) podAnnotWaitCond {
-	return func(pod *corev1.Pod, nadKey string) (*util.PodAnnotation, bool, error) {
-		// First, check the original annotation condition
-		annotation, isReady, err := annotCondFn(pod, nadKey)
-		if err != nil || !isReady {
-			return annotation, isReady, err
-		}
-		// primaryUDNPodRequest would be nil if no primary UDN interface is needed
-		primaryUDNPodRequest := pr.buildPrimaryUDNPodRequest(primaryUDN)
-		// DPU-Host: add DPU connection-details annotation to allow DPU performs the needed primary UDN interface plumbing.
-		if config.IsModeDPUHost() &&
-			primaryUDNPodRequest != nil && primaryUDNPodRequest.CNIConf.DeviceID != "" {
-			netdevName := primaryUDN.NetworkDevice()
-			if err := primaryUDNPodRequest.addDPUConnectionDetailsAnnot(k, podLister, netdevName); err != nil {
-				return annotation, false, err
-			}
-			// Check if DPU status annotation is ready (passing nil as we've already checked annotation)
-			return isDPUReady(nil, primaryUDN.NADName())(pod, nadKey)
-		}
-		// Non-DPU case: proceed normally
-		return annotation, true, nil
-	}
-}
-
-func (pr *PodRequest) cmdAddWithGetCNIResultFunc(
-	kubeAuth *KubeAPIAuth,
-	clientset *ClientSet,
-	getCNIResultFn getCNIResultFunc,
-	networkManager networkmanager.Interface,
-	ovsClient client.Client,
-) (*Response, error) {
+func (pr *PodRequest) cmdAdd(kubeAuth *KubeAPIAuth, clientset *ClientSet, ovsClient client.Client) (*Response, error) {
 	namespace := pr.PodNamespace
 	podName := pr.PodName
 	if namespace == "" || podName == "" {
@@ -170,14 +129,13 @@ func (pr *PodRequest) cmdAddWithGetCNIResultFunc(
 		return nil, fmt.Errorf("failed to get pod %s/%s: %v", namespace, podName, err)
 	}
 
-	if pr.netName != types.DefaultNetworkName {
+	// nadKey is only set for default network and primary UDN
+	if pr.nadKey == "" {
 		nadKey, err := GetCNINADKey(pod, pr.IfName, pr.nadName)
 		if err != nil {
 			return nil, fmt.Errorf("failed to get NAD key for CNI Add request %v: %v", pr, err)
 		}
 		pr.nadKey = nadKey
-	} else {
-		pr.nadKey = pr.nadName
 	}
 
 	annotCondFn := isOvnReady
@@ -202,15 +160,6 @@ func (pr *PodRequest) cmdAddWithGetCNIResultFunc(
 		// In the case of SmartNIC (CX5), we store the netdevname in the representor's
 		// OVS interface's external_id column. This is done in ConfigureInterface().
 	}
-	// Get the IP address and MAC address of the pod
-	// for DPU, ensure connection-details is present
-
-	primaryUDN := udn.NewPrimaryNetwork(networkManager, clientset.nadLister)
-	if util.IsNetworkSegmentationSupportEnabled() {
-		annotCondFn = primaryUDN.WaitForPrimaryAnnotationFn(annotCondFn)
-		// checks for primary UDN network's DPU connections status
-		annotCondFn = pr.primaryDPUReady(primaryUDN, kubecli, clientset.podLister, annotCondFn)
-	}
 
 	// now checks for default network's DPU connection status
 	if config.IsModeDPUHost() {
@@ -218,19 +167,12 @@ func (pr *PodRequest) cmdAddWithGetCNIResultFunc(
 			annotCondFn = isDPUReady(annotCondFn, pr.nadKey)
 		}
 	}
+
+	// Get the IP address and MAC address of the pod
+	// for DPU, ensure connection-details is present
 	pod, annotations, podNADAnnotation, err := GetPodWithAnnotations(pr.ctx, clientset, namespace, podName, pr.nadKey, annotCondFn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get pod annotation: %v", err)
-	}
-
-	var primaryUDNPodInfo *PodInterfaceInfo
-	primaryUDNPodRequest := pr.buildPrimaryUDNPodRequest(primaryUDN)
-	if primaryUDNPodRequest != nil {
-		primaryUDNPodInfo, err = primaryUDNPodRequest.buildPodInterfaceInfo(annotations, primaryUDN.Annotation(), primaryUDN.NetworkDevice())
-		if err != nil {
-			return nil, err
-		}
-		klog.V(4).Infof("Pod %s/%s primaryUDN podRequest %v podInfo %v", namespace, podName, primaryUDNPodRequest, primaryUDNPodInfo)
 	}
 
 	if err = pr.checkOrUpdatePodUID(pod); err != nil {
@@ -263,48 +205,14 @@ func (pr *PodRequest) cmdAddWithGetCNIResultFunc(
 			}
 		}
 
-		response.Result, err = getCNIResultFn(pr, clientset, podInterfaceInfo)
+		response.Result, err = getCNIResult(pr, clientset, podInterfaceInfo)
 		if err != nil {
 			return nil, err
 		}
-		if primaryUDNPodRequest != nil {
-			err = primaryUDNCmdAddGetCNIResultFunc(response.Result, getCNIResultFn, primaryUDNPodRequest, clientset, primaryUDNPodInfo)
-			if err != nil {
-				return nil, err
-			}
-		}
 	} else {
 		response.PodIFInfo = podInterfaceInfo
-		if primaryUDNPodRequest != nil {
-			response.PrimaryUDNPodInfo = primaryUDNPodInfo
-			response.PrimaryUDNPodReq = primaryUDNPodRequest
-		}
 	}
-
 	return response, nil
-}
-
-func primaryUDNCmdAddGetCNIResultFunc(result *current.Result, getCNIResultFn getCNIResultFunc, primaryUDNPodRequest *PodRequest,
-	clientset PodInfoGetter, primaryUDNPodInfo *PodInterfaceInfo) error {
-	primaryUDNResult, err := getCNIResultFn(primaryUDNPodRequest, clientset, primaryUDNPodInfo)
-	if err != nil {
-		return err
-	}
-
-	result.Routes = append(result.Routes, primaryUDNResult.Routes...)
-	numOfInitialIPs := len(result.IPs)
-	numOfInitialIfaces := len(result.Interfaces)
-	result.Interfaces = append(result.Interfaces, primaryUDNResult.Interfaces...)
-	result.IPs = append(result.IPs, primaryUDNResult.IPs...)
-
-	// Offset the index of the default network IPs to correctly point to the default network interfaces
-	for i := numOfInitialIPs; i < len(result.IPs); i++ {
-		ifaceIPConfig := result.IPs[i].Copy()
-		if result.IPs[i].Interface != nil {
-			result.IPs[i].Interface = current.Int(*ifaceIPConfig.Interface + numOfInitialIfaces)
-		}
-	}
-	return nil
 }
 
 func (pr *PodRequest) cmdDel(clientset *ClientSet) (*Response, error) {
@@ -470,42 +378,6 @@ func getCNIResult(pr *PodRequest, getter PodInfoGetter, podInterfaceInfo *PodInt
 		Interfaces: interfacesArray,
 		IPs:        ips,
 	}, nil
-}
-
-// buildPrimaryUDNPodRequest returns PodRequest for primary UDN interface,
-// it returns nil if primary UDN is not requested on the Pod
-func (pr *PodRequest) buildPrimaryUDNPodRequest(
-	primaryUDN *udn.UserDefinedPrimaryNetwork,
-) *PodRequest {
-	if !primaryUDN.Found() {
-		// if primary UDN interface is not needed, return nil
-		return nil
-	}
-	deviceID, deviceInfo, isVFIO := primaryUDN.NetworkDeviceInfo()
-	req := &PodRequest{
-		Command:      pr.Command,
-		PodNamespace: pr.PodNamespace,
-		PodName:      pr.PodName,
-		PodUID:       pr.PodUID,
-		SandboxID:    pr.SandboxID,
-		Netns:        pr.Netns,
-		IfName:       primaryUDN.InterfaceName(),
-		CNIConf: &ovncnitypes.NetConf{
-			// primary UDN MTU will be taken from config.Default.MTU
-			// if not specified at the NAD
-			MTU:      primaryUDN.MTU(),
-			DeviceID: deviceID,
-		},
-		timestamp:  time.Now(),
-		IsVFIO:     isVFIO,
-		netName:    primaryUDN.NetworkName(),
-		nadName:    primaryUDN.NADName(),
-		nadKey:     primaryUDN.NADName(),
-		deviceInfo: *deviceInfo,
-		ctx:        pr.ctx,
-	}
-
-	return req
 }
 
 func (pr *PodRequest) buildPodInterfaceInfo(annotations map[string]string, podAnnotation *util.PodAnnotation, netDevice string) (*PodInterfaceInfo, error) {

go-controller/pkg/cni/cni_suite_test.go
@@ -4,11 +4,20 @@
 package cni
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+func init() {
+	// Disable WatchListClient feature gate for tests.
+	// Fake clientsets from third-party libraries don't yet support WatchList semantics
+	// introduced in K8s 1.35, causing informers to hang waiting for bookmark events.
+	// See: https://github.com/kubernetes/kubernetes/issues/135895
+	os.Setenv("KUBE_FEATURE_WatchListClient", "false")
+}
 
 func TestCNISuite(t *testing.T) {
 	RegisterFailHandler(Fail)

commit 50a60a89aeec4a482c78e2405de2293265d1be6e

cni: wait for primary UDN annotation before configuring OVS port
Fix a race condition introduced in commit 1826e9971 ("Move primary UDN
out of pod request") that caused primary UDN pods to fail with "timed
out waiting for OVS port binding (ovn-installed)" errors.

The Problem:
When commit 1826e9971 refactored primary UDN handling to create a
separate pod request at the CNI server level, it removed the critical
annotation waiting logic. This created a race condition:

1. Default network CNI Add completes
2. CNI server creates primaryPodRequest for UDN interface
3. primaryPodRequest.cmdAdd() is called immediately
4. CNI tries to wait for external-ids:ovn-installed=true on OVS port
5. Race: OVN controller hasn't created the logical port yet
6. CNI times out after 2 minutes
7. Pod creation fails

The Root Cause:
The original code in udn/primary_network.go had WaitForPrimaryAnnotationFn()
which ensured the OVN controller had created the logical port and set
the pod annotation before the CNI attempted to configure OVS. When this
was refactored into cniserver.go, this waiting logic was not preserved.

The Fix:
Add a call to GetPodWithAnnotations(isOvnReady) after creating the
primaryPodRequest but before calling cmdAdd(). This ensures the OVN
controller has finished creating the logical port and setting the
annotation before the CNI attempts to configure the OVS interface.

This restores the original behavior where the CNI waits for the OVN
controller to be ready, eliminating the race condition.

Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

go-controller/pkg/cni/cniserver.go
@@ -333,6 +333,17 @@ func (s *Server) handleCNIRequest(r *http.Request) (result []byte, err error) {
 		}
 		if primaryPodRequest != nil {
 			klog.V(4).Infof("Pod %s/%s primaryUDN podRequest %v", primaryPodRequest.PodNamespace, primaryPodRequest.PodName, primaryPodRequest)
+
+			// Wait for the OVN controller to create the primary UDN annotation before proceeding.
+			// This prevents a race condition where the CNI tries to configure the OVS port
+			// before the logical port has been created by the OVN controller.
+			_, _, _, err = GetPodWithAnnotations(primaryPodRequest.ctx, s.clientSet,
+				primaryPodRequest.PodNamespace, primaryPodRequest.PodName,
+				primaryPodRequest.nadKey, isOvnReady)
+			if err != nil {
+				return nil, fmt.Errorf("failed to wait for primary UDN pod annotation: %v", err)
+			}
+
 			primaryResponse, err := primaryPodRequest.cmdAdd(s.kubeAuth, s.clientSet, s.ovsClient)
 			if err != nil {
 				return nil, fmt.Errorf("failed to add primary UDN pod request: %v", err)