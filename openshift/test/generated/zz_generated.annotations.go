2a5bcabcf79dd7c80347b1b156aeb3fdc5e73ca6
origin-release-container <origin-release@redhat.com>
2026-07-02 13:06:04 +0000

    sync test annotations with upstream changes
    - go mod vendor
    - ./openshift/hack/update-tests-annotation.sh
    
    Automated sync after downstream merge to keep test annotations
    in sync with upstream test modifications and rules.go changes.

--- openshift/test/generated/zz_generated.annotations.go
+++ openshift/test/generated/zz_generated.annotations.go
@@ -53,6 +53,10 @@ var AppendedAnnotations = map[string]string{
 
 	"ACL Logging for NetworkPolicy when the namespace's ACL logging annotation is updated the ACL logs are updated accordingly": "[Disabled:Unimplemented]",
 
+	"BGP: For BGP configured networks Validates BUM suppression is in effect for L2 EVPN networks MAC-VRF and IP-VRF": "[Suite:openshift/conformance/parallel]",
+
+	"BGP: For BGP configured networks Validates BUM suppression is in effect for L2 EVPN networks MAC-VRF only": "[Suite:openshift/conformance/parallel]",
+
 	"BGP: For BGP configured networks When the tested network is of type Layer 2 CUDN EVPN MAC-VRF and IP-VRF random VTEP When a pod runs on the tested network Can reach KAPI service": "[Suite:openshift/conformance/parallel]",
 
 	"BGP: For BGP configured networks When the tested network is of type Layer 2 CUDN EVPN MAC-VRF and IP-VRF random VTEP When a pod runs on the tested network It can be reached by an external server on the same network When the network is IPv4": "[Suite:openshift/conformance/parallel]",
