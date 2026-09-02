f44ab3fb3f0ee771216a8fdc65c62b6730aa4060
Anurag Saxena <anusaxen@redhat.com>
2026-09-02 09:45:31 -0400

    OTE: add OTE networkpolicy migration infra

--- openshift/cmd/annotate/main.go
+++ openshift/cmd/annotate/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/openshift/test/annotate"
 	_ "github.com/ovn-kubernetes/ovn-kubernetes/openshift/test/deploymentconfig"
+	_ "github.com/ovn-kubernetes/ovn-kubernetes/openshift/test/ote"
 )
 
 func main() {
