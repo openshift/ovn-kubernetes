6232e27f1b882d82ad0a78da2f0905c7b5b1d113
Periyasamy Palanisamy <pepalani@redhat.com>
2026-09-07 11:55:44 +0200

    OTE: write EgressIP yaml files to /tmp
    The test writes egressip.yaml and egressfirewall.yaml to the current
    working directory, which may not be writable. Use /tmp instead.
    
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>

--- test/e2e/egressip.go
+++ test/e2e/egressip.go
@@ -436,8 +436,8 @@ var _ = ginkgo.Describe("e2e egress IP validation", feature.EgressIP, ginkgo.Ser
 			targetNodeName          string = "egressTargetNode-allowed"
 			deniedTargetNodeName    string = "egressTargetNode-denied"
 			targetSecondaryNodeName string = "egressSecondaryTargetNode-allowed"
-			egressIPYaml            string = "egressip.yaml"
-			egressFirewallYaml      string = "egressfirewall.yaml"
+			egressIPYaml            string = "/tmp/egressip.yaml"
+			egressFirewallYaml      string = "/tmp/egressfirewall.yaml"
 			retryTimeout                   = 3 * retryTimeout // Boost the retryTimeout for EgressIP tests.
 		)
 
